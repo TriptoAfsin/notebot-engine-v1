32a96524b970c0b5d7ea8704dc00db19ba719ff4
TriptoAfsin <afsintripto@gmail.com>
2020-12-13 22:18:24 +0600

    notesflow ++

--- src/controllers/flows/botReplies/noteFlow.js
+++ src/controllers/flows/botReplies/noteFlow.js
@@ -1,34 +1,38 @@
 let notes =
 {
-    "attachment": {
-        "type": "template",
-        "payload": {
-            "template_type": "button",
-            "text": "Choose - ",
-            "buttons": [
-                {
-                    "type": "postback",
-                    "title": "Level 1",
-                    "payload": "level_1"
-                },
-                {
-                    "type": "postback",
-                    "title": "Level 2",
-                    "payload": "level_2"
-                },
-                {
-                    "type": "postback",
-                    "title": "Level 3",
-                    "payload": "level_3"
-                }
-            ],
+    "part1":{
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "button",
+                "text": "Choose - ",
+                "buttons": [
+                    {
+                        "type": "postback",
+                        "title": "Level 1",
+                        "payload": "level_1"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Level 2",
+                        "payload": "level_2"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Level 3",
+                        "payload": "level_3"
+                    }
+                ],
+            },
         },
-    },
-
+    }
+   
 }
 
 
 
 
 
-module.exports = notes;
+module.exports = {
+    notes_part1: notes.part1
+};
