7c07654b9efd45f5454a2bab3a531037aa3f39c7
TriptoAfsin <afsintripto@gmail.com>
2020-12-13 18:37:06 +0600

    btn fix

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -98,29 +98,39 @@ function handleMessage(sender_psid, received_message) {
 
     else if(notes.includes(received_message.text)){
         response = {
-            "text": `Choose -`,
-            "buttons": [
-                {
-                  "type": "postback",
-                  "title": "Level 1",
-                  "payload": "level_1",
-                },
-                {
-                  "type": "postback",
-                  "title": "Level 2",
-                  "payload": "level_2",
-                },
-                {
-                    "type": "postback",
-                    "title": "Level 3",
-                    "payload": "level_3",
-                },
-                {
-                    "type": "postback",
-                    "title": "Level 4",
-                    "payload": "level_4",
-                  }
-              ],
+            "attachment": {
+                "type": "template",
+                "payload": {
+                  "template_type": "generic",
+                  "elements": [{
+                    "title": "Choose",
+                    "subtitle": "",
+                    "buttons": [
+                      {
+                        "type": "postback",
+                        "title": "Level 1",
+                        "payload": "level_1",
+                      },
+                      {
+                        "type": "postback",
+                        "title": "Level 2",
+                        "payload": "level_2",
+                      },
+                      {
+                        "type": "postback",
+                        "title": "Level 3",
+                        "payload": "level_3",
+                      },
+                      {
+                        "type": "postback",
+                        "title": "Level 4",
+                        "payload": "level_4",
+                      }
+                    ],
+                  }]
+                }
+              }
+            }
           }
     }
 
