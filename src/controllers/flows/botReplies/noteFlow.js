e00fcdbd4485c63e275b88fe9dbc1f80c1261575
TriptoAfsin <afsintripto@gmail.com>
2020-12-13 21:27:21 +0600

    revert

--- src/controllers/flows/botReplies/noteFlow.js
+++ src/controllers/flows/botReplies/noteFlow.js
@@ -3,29 +3,25 @@ let notes =
     "attachment": {
         "type": "template",
         "payload": {
-            "template_type": "generic",
-            "elements": [
+            "template_type": "button",
+            "text": "Choose - ",
+            "buttons": [
                 {
-                    "title": "Choose",
-                    "buttons": [
-                        {
-                            "type": "postback",
-                            "title": "Level 1",
-                            "payload": "level_1"
-                        },
-                        {
-                            "type": "postback",
-                            "title": "Level 2",
-                            "payload": "level_2"
-                        },
-                        {
-                            "type": "postback",
-                            "title": "Level 3",
-                            "payload": "level_3"
-                        }
-                    ],
+                    "type": "postback",
+                    "title": "Level 1",
+                    "payload": "level_1"
+                },
+                {
+                    "type": "postback",
+                    "title": "Level 2",
+                    "payload": "level_2"
+                },
+                {
+                    "type": "postback",
+                    "title": "Level 3",
+                    "payload": "level_3"
                 }
-            ]
+            ],
         },
     },
 
