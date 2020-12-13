d01ace43eb85e06d9d5e865556726d36db3134c7
TriptoAfsin <afsintripto@gmail.com>
2020-12-13 16:42:31 +0600

    added psid

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -47,10 +47,14 @@ let postWebhook = (req, res) => {
         // Iterates over each entry - there may be multiple if batched
         body.entry.forEach(function(entry) {
 
-        // Gets the message. entry.messaging is an array, but 
-        // will only ever contain one message, so we get index 0
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
+            // Gets the message. entry.messaging is an array, but 
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
         });
 
     // Returns a '200 OK' response to all requests
