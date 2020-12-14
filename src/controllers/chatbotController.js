0cad4e455e8336bd6e3694e7f19597295b326537
TriptoAfsin <afsintripto@gmail.com>
2020-12-15 00:18:42 +0600

    reborn

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -132,7 +132,7 @@ function handleMessage(sender_psid, received_message) {
 
     
   if (received_message.text !== undefined || received_message.text !== '') {
-    /*
+    
     // Check if the message
     if (greets.some(word => received_message.text.toLowerCase().includes(word))) {
       // Create the payload for a basic text message
@@ -182,7 +182,7 @@ function handleMessage(sender_psid, received_message) {
     }
 
 
-    */
+    
     //default reply
     if (received_message.text) {
       // Create the payload for a basic text message
