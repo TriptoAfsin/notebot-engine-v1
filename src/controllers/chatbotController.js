334149b590b92457fadad1199c909c08d90a2748
TriptoAfsin <afsintripto@gmail.com>
2020-12-14 19:41:36 +0600

    atatchments

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -173,6 +173,36 @@ function handleMessage(sender_psid, received_message) {
       // Create the payload for a basic text message
       response = defaultReply[0];
     }  
+
+    else if (received_message.attachments) {
+      // Get the URL of the message attachment
+      let attachment_url = received_message.attachments[0].payload.url;
+      response = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "title": "Is this the right picture?",
+              "subtitle": "Tap a button to answer.",
+              "image_url": attachment_url,
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Yes!",
+                  "payload": "yes",
+                },
+                {
+                  "type": "postback",
+                  "title": "No!",
+                  "payload": "no",
+                }
+              ],
+            }]
+          }
+        }
+      }
+    } 
     
     // Sends the response message
     callSendAPI(sender_psid, response);    
