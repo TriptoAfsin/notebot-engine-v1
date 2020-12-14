5410d0a16b50a5327fc35d1fd117aa665bb02456
TriptoAfsin <afsintripto@gmail.com>
2020-12-14 23:28:26 +0600

    ++

--- src/controllers/flows/botReplies/welcome/getStarted.js
+++ src/controllers/flows/botReplies/welcome/getStarted.js
@@ -1,11 +1,36 @@
 //services
-const chatBotService = require('../../../../services/chatBotService');
+require("dotenv").config();
 
-let username = chatBotService.getFacebookUserInfo(sender_psid);
+
+const request = require('request');
+
+let userName = '';
+
+let getFacebookUserInfo = (sender_psid) => {
+
+    return new Promise((resolve, reject) => {
+        let uri = `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${process.env.PAGE_ACCESS_TOKEN}`
+        // Send the HTTP request to the Messenger Platform
+        request({
+            "uri": uri,
+            "method": "GET", //get method here
+        }, (err, res, body) => {
+            //console.log(body);
+            if (!err) {
+                userName = `${body.last_name} ${body.first_name}`;
+                resolve(userName);
+            } else {
+                reject("Unable to send message:" + err);
+            }
+        });
+    })
+}
+
+
 
 const getStarted = [
     {
-        "text": `Hello ${username}, Welcome to the new BUTEX NoteBOT`,
+        "text": `Hello ${getFacebookUserInfo}, Welcome to the new BUTEX NoteBOT`,
         "quick_replies": [{
                 "content_type": "text",
                 "title": "Help😥",
