b662c3fe3da4bf82384fe417d6ffe3b0be21deaf
TriptoAfsin <afsintripto@gmail.com>
2020-12-17 23:21:41 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/bce/topics/allsheetsBce.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/bce/topics/allsheetsBce.js
@@ -1,12 +1,10 @@
 let allsheetBce = [
     {
-        "text": `1. All Sheats Combined (Adnan Sir) - \n\n
-
+        "text": `1. All Sheats Combined (Adnan Sir) - \n
         https://drive.google.com/file/d/1Ks9AVvqGxOHL3GMiSvOc9zyv0uh7ujYV/view?usp=sharing`
     },
     {
-        "text": `2. All Sheet(Tripti Mam)- \n\n
-
+        "text": `2. All Sheet(Tripti Mam)- \n
         https://drive.google.com/drive/u/0/folders/187M5v8F3WIpAQ_7NolzBm2b9VsncHguc`
     },
    
