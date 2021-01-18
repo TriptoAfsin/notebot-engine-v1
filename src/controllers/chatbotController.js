1f8c4271dd9d8485ef50a80f3091f878759f066c
TriptoAfsin <afsintripto@gmail.com>
2021-01-18 23:59:53 +0600

    ++

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1474,7 +1474,7 @@ let postWebhook = (req, res) => {
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
-      console.log('WebHook Event:'+webhook_event);
+      console.log('WebHook Event Msg:'+webhook_event.message.text);
 
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
