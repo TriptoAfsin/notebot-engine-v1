51e1c32626416dc1faec5537d0c779639bdee929
TriptoAfsin <afsintripto@gmail.com>
2021-02-09 01:31:58 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/fsd/fsd_flow.js
+++ src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/fsd/fsd_flow.js
@@ -9,7 +9,6 @@ let fsdFlow = [
         [
             payloadBtnGen("Intro to FSD", "fsd_intro_flow"),
             payloadBtnGen("Plain Weave", "fsd_plainWeave_flow"),
-            payloadBtnGen("Test", "ACT::052c22a603140979cbe8a3f3f32fc159")
         ]
     )
 ]
