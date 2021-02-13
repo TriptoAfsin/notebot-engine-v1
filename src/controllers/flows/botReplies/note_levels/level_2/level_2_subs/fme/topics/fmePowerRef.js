c268579d79c5ce8b272502d7c241f55e54485cad
TriptoAfsin <afsintripto@gmail.com>
2021-02-14 03:06:59 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/fme/topics/fmePowerRef.js
+++ src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/fme/topics/fmePowerRef.js
@@ -2,9 +2,9 @@ let textBlockGen = require("../../../../../../../genrators/textBlockGen");
 
 
 let fmePower = [
-    textBlockGen(`🔷Sheet (Hamja Sir, 2020)-\n\nhttps://drive.google.com/file/d/1R8bfVaiAQCjBbBGNV4USAR2ZdJxeFPm4/view?usp=sharing`),
-    textBlockGen(`Hand Note (Tripto, 2020)-\n\nhttps://drive.google.com/file/d/1DF_JNVIqPvzMMfRv2SqMEDebDgKkbVSL/view?usp=sharing`),
-    textBlockGen(`Hand Note on Radiation (Tripto, 2020)-\n\nhttps://drive.google.com/file/d/11vEMMD_DV-nuNwP1TqkBw_3nGtgGcO3L/view?usp=sharing`)
+    textBlockGen(`🔷 Sheet (Hamja Sir, 2020)-\n\nhttps://drive.google.com/file/d/1R8bfVaiAQCjBbBGNV4USAR2ZdJxeFPm4/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note (Tripto, 2020)-\n\nhttps://drive.google.com/file/d/1DF_JNVIqPvzMMfRv2SqMEDebDgKkbVSL/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note on Radiation (Tripto, 2020)-\n\nhttps://drive.google.com/file/d/11vEMMD_DV-nuNwP1TqkBw_3nGtgGcO3L/view?usp=sharing`)
 ]
 
 
--- src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/fme/topics/fmeSteamTurb.js
+++ src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/fme/topics/fmeSteamTurb.js
@@ -2,8 +2,8 @@ let textBlockGen = require("../../../../../../../genrators/textBlockGen");
 
 
 let fmeSteam = [
-    textBlockGen(`🔷Sheet(Hamja Sir, 2020)-\n\nhttps://drive.google.com/file/d/1TdEacikyeousOS9hj8CSMCthuRw567By/view?usp=sharing`),
-    textBlockGen(`Hand Note (Tripto, 2020)-\n\nhttps://drive.google.com/file/d/1PbMKBgfblKzZke48pJiPsJ5snQxkWtxu/view?usp=sharing`),
+    textBlockGen(`🔷 Sheet(Hamja Sir, 2020)-\n\nhttps://drive.google.com/file/d/1TdEacikyeousOS9hj8CSMCthuRw567By/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note (Tripto, 2020)-\n\nhttps://drive.google.com/file/d/1PbMKBgfblKzZke48pJiPsJ5snQxkWtxu/view?usp=sharing`),
 ]
 
 
