e50567af9b4987ed0531984e0efe43916580d1a4
TriptoAfsin <afsintripto@gmail.com>
2021-02-24 21:35:33 +0600

    ++

--- src/controllers/genrators/webBtnBlockGen.js
+++ src/controllers/genrators/webBtnBlockGen.js
@@ -1,13 +1,11 @@
-let lengthValidator = require('./validators/lengthValidator');
+const lengthValidator = require("./validators/lengthValidator");
 
 let webBtnBlockGen = (title, url) => {
-
     return {
         "type": "web_url",
-        "title":  lengthValidator(title, 15),
-        "payload": url
+        "url": url,
+        "title": lengthValidator(title, 15),
     }
-    
 }
 
 module.exports = webBtnBlockGen;
