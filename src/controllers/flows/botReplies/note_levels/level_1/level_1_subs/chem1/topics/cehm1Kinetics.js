94ec7aebc67736ec1d56bc9fcb528b1af6ce8ed7
TriptoAfsin <afsintripto@gmail.com>
2021-03-05 21:25:40 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem1/topics/cehm1Kinetics.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem1/topics/cehm1Kinetics.js
@@ -1,21 +1,12 @@
+let textBlockGen = require("../../../../../../../genrators/textBlockGen");
+
+
 let chem1Kinetics = [
-    {
-        "text": `🔷Hand Note(Tripto, 2018)-
-        https://drive.google.com/file/d/1I6jA6ZBSSqmIspWoscNytSMqKLwJuyAB/view?usp=sharing`
-    },
-    {
-        "text": `🔷Hand Note(Akib, 2018) - 
-        https://drive.google.com/file/d/1TJ-EJS4XBr0ZZv2ei-dRv6cwyRcGOu0-/view?usp=drivesdk`
-    },
-    {
-        "text": `🔷 Slide(2019)-
-        https://drive.google.com/file/d/1Tp8K8LazObsV9dVXBOaooXoeelsNoCUz/view?usp=sharing`
-    },
-    {
-        "text": `🔷Sheet(2019)-
-        https://drive.google.com/file/d/1-46QhXpDeEICp0jthkxzMIcD0G9_1Uf8/view?usp=sharing`
-    }
-   
+    textBlockGen(`🔷 Hand Note(Tripto, 2018)-\n\nhttps://drive.google.com/file/d/1I6jA6ZBSSqmIspWoscNytSMqKLwJuyAB/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Akib, 2018) - \n\nhttps://drive.google.com/file/d/1TJ-EJS4XBr0ZZv2ei-dRv6cwyRcGOu0-/view?usp=drivesdk`),
+    textBlockGen(`🔷 Hand Note(Azim,TEM-46 ,2021) - \n\nhttps://drive.google.com/file/d/19AAenPq3U0mU-Y7ouX45plplgzxd1q4A/view?usp=sharing`),
+    textBlockGen(`🔷 Slide(2019)-\n\nhttps://drive.google.com/file/d/1Tp8K8LazObsV9dVXBOaooXoeelsNoCUz/view?usp=sharing`),
+    textBlockGen(`🔷 Sheet(2019)-\n\nhttps://drive.google.com/file/d/1-46QhXpDeEICp0jthkxzMIcD0G9_1Uf8/view?usp=sharing`),
 ]
 
 
--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem1/topics/chem1Coll.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem1/topics/chem1Coll.js
@@ -3,7 +3,7 @@ let textBlockGen = require("../../../../../../../genrators/textBlockGen");
 let chem1Coll = [
     textBlockGen(`🔷 Hand Note(Akib, 2018) - \n\nhttps://drive.google.com/file/d/1Sc-oUR7Hxm7W0t5xEuQg9fhj4z1dar8m/view?usp=drivesdk`),
     textBlockGen(`🔷 Hand Note(Tripto, 2018)-\n\nhttps://drive.google.com/file/d/17GLC4UsQ8ICub3TQO_mRpQHx3l56_W0P/view?usp=sharing`),
-    textBlockGen(`🔷 Hand Note(Azim, TEM-46 ,2021)-\n\nhttps://drive.google.com/file/d/1KoUfnj1tI7EAGHNxjA_WvglTgL-NTf2H/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Azim, TEM-46 ,2021)-\n\nhttps://drive.google.com/file/d/1ZaNcg2UxxyDi6MsZH7DGwwzWrhulD1io/view?usp=sharing`),
     textBlockGen(`🔷 Sheet(2019)-\n\nhttps://drive.google.com/file/d/1282uOhZZiJvaM27pQQaC6iipU8E3Plvk/view?usp=sharing`)
 ]
 
