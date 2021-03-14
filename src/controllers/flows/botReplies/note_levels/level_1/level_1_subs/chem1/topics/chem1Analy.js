794bac03ed21799bb5bac75609bf034803a06eeb
TriptoAfsin <afsintripto@gmail.com>
2021-03-15 00:58:57 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem1/topics/chem1Analy.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem1/topics/chem1Analy.js
@@ -1,25 +1,13 @@
+let textBlockGen = require("../../../../../../../genrators/textBlockGen");
+
+
 let chem1Annaly = [
-    {
-        "text": `🔷Farhad Sir Sheet(2018) - 
-        https://drive.google.com/file/d/1zmbcbYl-l-c91vo21prcLuwSio9OENSY/view?usp=sharing`
-    },
-    {
-        "text": `🔷 Hand Note(Akib, 2018) - 
-        https://drive.google.com/file/d/1NpOB4QNmBaL2pBJ5hexlHrNYdtId54bo/view?usp=drivesdk`
-    },
-    {
-        "text": `🔷 Sampling(Rafsan, 2019) -
-        https://drive.google.com/file/d/1uj68ZjgXtWnz_oSJfEhgqZh08XPYxOFz/view?usp=sharing`
-    },
-    {
-        "text": `🔷Mathematical Terms(Rafsan, 2019)-
-        https://drive.google.com/file/d/1c2DV8OoiOyYOiwsg_OLHyKvAAusBqivt/view?usp=sharing`
-    },
-    {
-        "text": `🔷Hand Note(Oindrela, 2019)-
-        https://drive.google.com/file/d/1ZkIOAUpbGaGf2xsNAT28ZUJUVRu2TC7n/view?usp=sharing`
-    }
-   
+    textBlockGen(`🔷 Farhad Sir Sheet(2018) - \n\nhttps://drive.google.com/file/d/1zmbcbYl-l-c91vo21prcLuwSio9OENSY/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Akib, 2018) - \n\nhttps://drive.google.com/file/d/1NpOB4QNmBaL2pBJ5hexlHrNYdtId54bo/view?usp=drivesdk`),
+    textBlockGen(`🔷 Hand Note(Azim,TEM-46,2021) - \n\nhttps://drive.google.com/file/d/1MX-66ltdq95IlEs_nJ0QkeHDaMovUni3/view?usp=sharing`),
+    textBlockGen(`🔷 Sampling(Rafsan, 2019) -\n\nhttps://drive.google.com/file/d/1uj68ZjgXtWnz_oSJfEhgqZh08XPYxOFz/view?usp=sharing`),
+    textBlockGen(`🔷 Mathematical Terms(Rafsan, 2019)-\n\nhttps://drive.google.com/file/d/1c2DV8OoiOyYOiwsg_OLHyKvAAusBqivt/view?usp=sharing`),
+    textBlockGen(`🔷Hand Note(Oindrela, 2019)-\n\nhttps://drive.google.com/file/d/1ZkIOAUpbGaGf2xsNAT28ZUJUVRu2TC7n/view?usp=sharing`),
 ]
 
 
