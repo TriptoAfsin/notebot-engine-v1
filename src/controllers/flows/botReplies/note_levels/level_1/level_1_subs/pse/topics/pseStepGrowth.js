3eb2c0a4538f793194f8afdefbae7b0c97aafb9b
TriptoAfsin <afsintripto@gmail.com>
2021-04-01 15:09:19 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/pse/topics/pseStepGrowth.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/pse/topics/pseStepGrowth.js
@@ -5,6 +5,7 @@ let pseStepGrowth = [
     textBlockGen(`🔷 Hand Note(Akib, 2018) - \n\nhttps://drive.google.com/file/d/1yWwU-VbwU-tA7ZS59gih9g03Pzr--rx6/view?usp=drivesdk`),
     textBlockGen(`🔷 Hand Note(Tripto, 2018) - \n\nhttps://drive.google.com/file/d/1nyPGEIfYdE5l3-_mLeXVQ6uD1Kh9PSdm/view?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Urmi,TEM-45, 2021) - \n\nhttps://drive.google.com/file/d/1ktusoW-8EXINPVefoBit-dqFNk3Rzgbh/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Nafis, IPE-46, 2021) - \n\nhttps://drive.google.com/file/d/1Y__JPMSpR8m1P8N0mkUzdoing81qKJKi/view?usp=sharing`),
     textBlockGen(`🔷 Book Scanned(Mamun Sir) - \n\nhttps://drive.google.com/file/d/1-toD7hURheNvaBeoEpRipXnnp4gyvFnc/view?usp=drivesdk`),
 ]
 
