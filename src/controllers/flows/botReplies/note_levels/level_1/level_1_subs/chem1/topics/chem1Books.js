f8d50c62821fb0aed99261669aba6fd338fb5805
TriptoAfsin <afsintripto@gmail.com>
2021-04-18 14:55:30 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem1/topics/chem1Books.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem1/topics/chem1Books.js
@@ -1,22 +1,22 @@
 let chem1Books = [
     {
-        "text": `🔷Inorganic Chemistry(J.D Lee) - 
+        "text": `🔷 Inorganic Chemistry(J.D Lee) - 
         https://drive.google.com/file/d/1iaXX15sPYGb1xosXdNyv-fkk5nX3kjEe/view?usp=sharing`
     },
     {
-        "text": `🔷Essential of Physical Chemistry(Bahl & Tuli) - 
+        "text": `🔷 Essential of Physical Chemistry(Bahl & Tuli) - 
         https://drive.google.com/file/d/1Te7-xJ5dP4aEK5vSz43lonq_JyfSRJtr/view?usp=sharing`
     },
     {
-        "text": `🔷Essentials of Physical Chemistry(S Chand) - 
+        "text": `🔷 Essentials of Physical Chemistry(S Chand) - 
         https://drive.google.com/file/d/1GJiAT76ibzUTemWDeyUFS6pza6ZJiucM/view?usp=drivesdk`
     },
     {
-        "text": `🔷General Chemistry (Ebbing) - 
+        "text": `🔷 General Chemistry (Ebbing) - 
         https://drive.google.com/file/d/1iw9YEPnfDIiHGPiBO0S-s5JR6VCKVL9v/view?usp=drivesdk`
     },
     {
-        "text": `🔷Chemistry(Raymond Chang)-
+        "text": `🔷 Chemistry(Raymond Chang)-
         https://drive.google.com/file/d/1WzTGWtqKbptJ11SHz5qO2Lab5lmPiukL/view?usp=sharing`
     }
    
