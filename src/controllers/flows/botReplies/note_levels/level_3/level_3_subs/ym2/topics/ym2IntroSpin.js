8c24bdf04a81e09f8f29b507a3cdc2fe033ff627
TriptoAfsin <afsintripto@gmail.com>
2021-04-27 16:08:49 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/ym2/topics/ym2IntroSpin.js
+++ src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/ym2/topics/ym2IntroSpin.js
@@ -1,7 +1,7 @@
 let textBlockGen = require("../../../../../../../genrators/textBlockGen");
 
 let ym2Intro = [
-    textBlockGen(`🔷 Hand Note(Maruf, DCE-44, 2021) - https://drive.google.com/file/d/19lqtqpeIydmJgLa2U4Ncp_Da8Ow8Dv4s/view?usp=sharing`)
+    textBlockGen(`🔷 Hand Note(Maruf, DCE-44, 2021) - \n\nhttps://drive.google.com/file/d/19lqtqpeIydmJgLa2U4Ncp_Da8Ow8Dv4s/view?usp=sharing`)
     
 ]
 
