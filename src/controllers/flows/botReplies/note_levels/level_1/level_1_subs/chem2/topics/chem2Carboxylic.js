2871d760663739e2b33ade7c439db18002ff3540
TriptoAfsin <afsintripto@gmail.com>
2021-04-30 15:57:55 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem2/topics/chem2Carboxylic.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem2/topics/chem2Carboxylic.js
@@ -1,33 +1,18 @@
-let chem1Bond = [
-    {
-        "text": `🔷Hand Note(Tripto, based on Forhad Sir's Lecture,2018)-
-        https://drive.google.com/file/d/13axAYJWl0sugB1adlMiaPI87O0BufVPD/view?usp=sharing`
-    },
-    {
-        "text": `🔷 HandNote(Nahid, 2019,Based on Farhad Sir's Lecture)-
-        https://drive.google.com/file/d/1-MB3748bT9G8THTId5TJCvlTbZ18CfQ7/view?usp=sharing`
-    },
-    {
-        "text": `🔷 Hand Note (Tasnova,based on Forhad Sir's Lecture, 2019)-
-        https://drive.google.com/file/d/1_uBKjBtgHTMc2FSTQqhJeX5QumXcDEoX/view?usp=sharing`
-    },
-    {
-        "text": `🔷 Slide on Carboxylic Acid - 
-        https://drive.google.com/file/d/17zpL0NlvSjyABI-Jd_hXtZONO-jZWvdy/view?usp=sharing`
-    },
-    {
-        "text": `🔷 Carboxylic Acid lecture(Forhad Sir)-
-        https://drive.google.com/file/d/13L7C9BY_pJ1YVpSldeK8jGTxht-6RutN/view?usp=sharing`
-    },
-    {
-        "text": `🔷  Carboxylic Acid Book Scanned - 
-        https://drive.google.com/file/d/0B0FdOS_BOGE3djU3RXk3dlZWR2M/view?usp=sharing`
-    }
-   
+let textBlockGen = require("../../../../../../../genrators/textBlockGen");
+
+
+let chem1Carboxylic = [
+    textBlockGen(`🔷 Hand Note(Tripto, based on Forhad Sir's Lecture,2018)-\n\nhttps://drive.google.com/file/d/13axAYJWl0sugB1adlMiaPI87O0BufVPD/view?usp=sharing`),
+    textBlockGen(`🔷 HandNote(Nahid, 2019,Based on Farhad Sir's Lecture)-\n\nhttps://drive.google.com/file/d/1-MB3748bT9G8THTId5TJCvlTbZ18CfQ7/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note (Tasnova,based on Forhad Sir's Lecture, 2019)-\n\nhttps://drive.google.com/file/d/1_uBKjBtgHTMc2FSTQqhJeX5QumXcDEoX/view?usp=sharing`),
+    textBlockGen(`🔷 Slide on Carboxylic Acid - \n\nhttps://drive.google.com/file/d/17zpL0NlvSjyABI-Jd_hXtZONO-jZWvdy/view?usp=sharing`),
+    textBlockGen(`🔷 Carboxylic Acid lecture(Forhad Sir)-\n\nhttps://drive.google.com/file/d/13L7C9BY_pJ1YVpSldeK8jGTxht-6RutN/view?usp=sharing`),
+    textBlockGen(`🔷 Azmal Sir's Sheet-\n\nhttps://drive.google.com/file/d/1yK7niDAPRa19x7ytWq6_5r2li_BfJ5My/view?usp=sharing`),
+    textBlockGen(`🔷 Carboxylic Acid Book Scanned - \n\nhttps://drive.google.com/file/d/0B0FdOS_BOGE3djU3RXk3dlZWR2M/view?usp=sharing`),
 ]
 
 
 
 
 
-module.exports = chem1Bond;
+module.exports = chem1Carboxylic;
