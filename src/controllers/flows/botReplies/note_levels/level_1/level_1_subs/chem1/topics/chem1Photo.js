3a89553fbac368e64b601c97b648a0ea42554c98
TriptoAfsin <afsintripto@gmail.com>
2021-05-09 19:45:01 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem1/topics/chem1Photo.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem1/topics/chem1Photo.js
@@ -4,6 +4,7 @@ let chem1Photo = [
     textBlockGen(`🔷 Sheet(Sadia Mam, 2019)-\n\nhttps://drive.google.com/file/d/1ZPS17PD9xnKztAsWUnT04zuWw_AE2hg8/view?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Tripto, 2018)-\n\nhttps://drive.google.com/file/d/16-oVfmkz-0NZoyz_DcHsDlMpTRGZ-hi6/view?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Azim, TEM-46,2020)-\n\nhttps://drive.google.com/file/d/163wx4jWHU1nFFoakmY22JUn3LGsQK7x4/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Fiza, AE-46,2021)-\n\nhttps://drive.google.com/file/d/1Z0-s6CRnnlZB4r8v39bn5VeS-q7l2S4e/view?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Mahfuz, YE-46,2021)-\n\nhttps://drive.google.com/file/d/1rSbcURsACwy3TvfQh5_IYPOOZxzrHUPD/view?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Nafis, IPE-46,2020)-\n\nhttps://drive.google.com/file/d/1msasoTT1LEDG4w1rkpi4de2GNNpW8AvJ/view?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Nihat, 46,2021)-\n\nhttps://drive.google.com/file/d/1xtulOxDkVNkFYLKYLb1ZqqmLS8SB12hr/view?usp=sharing`),
