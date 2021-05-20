16483e931ab11b996fc62c3052be83086b37e865
TriptoAfsin <afsintripto@gmail.com>
2021-05-20 15:42:32 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/em/topics/emComposites.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/em/topics/emComposites.js
@@ -1,8 +1,8 @@
+let textBlockGen = require("../../../../../../../genrators/textBlockGen");
+
 let emComposites = [
-    {
-        "text": `🔷Composite(Slide, 2019)-
-        https://drive.google.com/file/d/1qOjV0SyDvwEZNnhIuN1QMu_lDYTmCEG-/view?usp=sharing`
-    }
+    textBlockGen(`🔷 Hand Note(Nafis, IPE-46, 2021)-\n\nhttps://drive.google.com/file/d/1KvUkN8C2dymvYFIOIIvmPs7te_ES7Vn3/view?usp=sharing`),
+    textBlockGen(`🔷 Composite(Slide, 2019)-\n\nhttps://drive.google.com/file/d/1qOjV0SyDvwEZNnhIuN1QMu_lDYTmCEG-/view?usp=sharing`),
 ]
 
 
