545f4c099c120bb26b778bcdbb20afedf16dc3c4
TriptoAfsin <afsintripto@gmail.com>
2021-06-04 16:04:35 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/math1/topics/math1Linear.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/math1/topics/math1Linear.js
@@ -1,13 +1,10 @@
+let textBlockGen = require("../../../../../../../genrators/textBlockGen");
+
+
 let math1Linear = [
-    {
-        "text": `🔷 Hand Note(Sazzad, 2019)-
-        https://drive.google.com/file/d/1AhGHMSc80C3Dl7LnzFnjCgKUTKYXYCbe/view?usp=sharing`
-    },
-    {
-        "text": `🔷Class Lecture(Brishty, 2019)-
-        https://drive.google.com/file/d/1-JKxx9lNGbINdVz58rZnjHD_ZHJZ4XNG/view`
-    }
-   
+    textBlockGen(`🔷 Hand Note(Sazzad, 2019)-\n\nhttps://drive.google.com/file/d/1AhGHMSc80C3Dl7LnzFnjCgKUTKYXYCbe/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Tanima, DCE-46, 2021)-\n\nhttps://drive.google.com/file/d/1KbR9MOoRgzFnxRAPegm05qq4v3K_qLct/view?usp=sharing`),
+    textBlockGen(`🔷Class Lecture(Brishty, 2019)-\n\nhttps://drive.google.com/file/d/1-JKxx9lNGbINdVz58rZnjHD_ZHJZ4XNG/view`),
 ]
 
 
