6e45c7f2d606183f68d279601e00047ea8fdc2cd
TriptoAfsin <afsintripto@gmail.com>
2021-06-08 12:31:47 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem2/topics/chem2CarboHy.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem2/topics/chem2CarboHy.js
@@ -1,24 +1,12 @@
+let textBlockGen = require("../../../../../../../genrators/textBlockGen");
+
+
 let chem2carbohydrates = [
-    {
-        "text": `🔷Farhad Sir Sheet(2019)-
-        https://drive.google.com/file/d/1oxLP2jZ4Uh6HBDbcj94gFttZOJRT1fbh/view?usp=sharing`
-    },
-    {
-        "text": `🔷 Hand Note(Tripto, 2019)-
-        https://drive.google.com/file/d/133QDY81TTxwV-infrGmIRErFNdKGroNV/view?usp=sharing`
-    },
-    {
-        "text": `🔷 Hand Note(Nahid ,2019, Based on Forhad Sir's Lecture)-
-        https://drive.google.com/file/d/1vLexj0_7l794hUxnk1g2Ki8kNOEPxlsJ/view?usp=sharing`
-    },
-    {
-        "text": `🔷 Sheet on Carbohydrates(Azmol Sir,2018) - 
-        https://drive.google.com/file/d/1_XgC2dUYJwrPdvz4u4BrgNzU_7Fc7Cf9/view?usp=sharing`
-    },
-    {
-        "text": `🔷 Class Lecture(Forhad Sir) - 
-        https://drive.google.com/file/d/1M_Y8si8pF2ndoVLv1ViReaHqsYBzzu_q/view?usp=sharing`
-    }
+    textBlockGen(`🔷 Farhad Sir Sheet(2019)-\n\nhttps://drive.google.com/file/d/1oxLP2jZ4Uh6HBDbcj94gFttZOJRT1fbh/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Tripto, 2019)-\n\nhttps://drive.google.com/file/d/133QDY81TTxwV-infrGmIRErFNdKGroNV/view?usp=sharing`),
+    textBlockGen(`🔷 Sheet on Carbohydrates(Azmol Sir,2018) - \n\nhttps://drive.google.com/file/d/1_XgC2dUYJwrPdvz4u4BrgNzU_7Fc7Cf9/view?usp=sharing`),
+    textBlockGen(`🔷 Class Lecture(Forhad Sir) - \n\nhttps://drive.google.com/file/d/1M_Y8si8pF2ndoVLv1ViReaHqsYBzzu_q/view?usp=sharing`),
+    textBlockGen(`🔴 A Note(Nahid, 2019) was removed from the platform for containing numerous errors`),
 ]
 
 
