5f900a1edff1763f1467871ab0aaac29ed35ff31
TriptoAfsin <afsintripto@gmail.com>
2021-06-12 00:27:09 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/am1/am1_flow.js
+++ src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/am1/am1_flow.js
@@ -1,160 +1,64 @@
+let grroupedButtonBlockGen = require("../../../../../../genrators/grroupedButtonBlockGen");
+let webBtnBlockGen = require("../../../../../../genrators/webBtnBlockGen");
+let payloadBtnGen = require("../../../../../../genrators/payloadBtnGen");
+
+
+
 let am1Flow = [
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "📌 Full Hand Note & Slide (2020) -  ",
-                "buttons": [
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1MSe7Mc7MlqLCIziwBa3iTJQb6nkYRgM-/view",
-                        "title": "Part A (Rifat)",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1BtjAiJBU0eL-UkM66Rr3V7n5FUcWMhUx/view",
-                        "title": "Part B (Rifat)",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1nO_R7oC3MW_Ze9lQS1RjfIKBUrY_SB2M/view",
-                        "title": "Slide Part A",
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for AM-I - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Books",
-                        "payload": "am1_books_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Hand Notes",
-                        "payload": "am1_hnotes_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Questions",
-                        "payload": "am1_ques_flow"
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for AM-I - ",
-                "buttons": [
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1oUqikuBVJr5l4LTRF8NAhFe5J1CBMt-D/view",
-                        "title": "Basic Terms(Tripto)",
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Intro",
-                        "payload": "am1_intro_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Structure of Tex In",
-                        "payload": "am1_structure_tex_flow"
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for AM-I - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Sizing System",
-                        "payload": "am1_sizing_sys_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Seam & Stitch",
-                        "payload": "am1_seam_stitch_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Trims & Acces..",
-                        "payload": "am1_trim_acces_flow"
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for AM-I - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Interlining",
-                        "payload": "am1_interlining_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Pattern",
-                        "payload": "am1_pattern_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Marker",
-                        "payload": "am1_marker_flow"
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for AM-I - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Spreading",
-                        "payload": "am1_spreading_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Fabric Cutting",
-                        "payload": "am1_fabric_cut_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Fabric Inspection",
-                        "payload": "am1_fabricInspection_flow"
-                    }
-                ],
-            },
-        },
-    }
+    grroupedButtonBlockGen(
+        "📌 Full Hand Note & Slide (2020) -",
+        [
+            webBtnBlockGen("Part A (Rifat)", "https://drive.google.com/file/d/1MSe7Mc7MlqLCIziwBa3iTJQb6nkYRgM-/view"),
+            webBtnBlockGen("Part B (Rifat)", "https://drive.google.com/file/d/1BtjAiJBU0eL-UkM66Rr3V7n5FUcWMhUx/view"),
+            webBtnBlockGen("Slide Part A", "https://drive.google.com/file/d/1nO_R7oC3MW_Ze9lQS1RjfIKBUrY_SB2M/view"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "📌 Full Hand Notes",
+        [
+            webBtnBlockGen("Maruf(2021)", "https://drive.google.com/file/d/1PtuUUaa1EbmazOEpa6GyjEuBEpr2d2zl/view?usp=sharing"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for AM-I -",
+        [
+            payloadBtnGen("Books", "am1_books_flow"),
+            payloadBtnGen("Hand Notes", "am1_hnotes_flow"),
+            payloadBtnGen("Questions", "am1_ques_flow"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for AM-I -",
+        [
+            webBtnBlockGen("Basic Terms(Tripto)", "https://drive.google.com/file/d/1oUqikuBVJr5l4LTRF8NAhFe5J1CBMt-D/view"),
+            payloadBtnGen("Intro", "am1_intro_flow"),
+            payloadBtnGen("Structure of Tex In", "am1_structure_tex_flow"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for AM-I -",
+        [
+            payloadBtnGen("Sizing System", "am1_sizing_sys_flow"),
+            payloadBtnGen("Seam & Stitch", "am1_seam_stitch_flow"),
+            payloadBtnGen("Trims & Acces..", "am1_trim_acces_flow"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for AM-I -",
+        [
+            payloadBtnGen("Interlining", "am1_interlining_flow"),
+            payloadBtnGen("Pattern", "am1_pattern_flow"),
+            payloadBtnGen("Marker", "am1_marker_flow"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for AM-I -",
+        [
+            payloadBtnGen("Spreading", "am1_spreading_flow"),
+            payloadBtnGen("Fabric Cutting", "am1_fabric_cut_flow"),
+            payloadBtnGen("Fabric Inspection", "am1_fabricInspection_flow"),
+        ]
+    ), 
 ]
 
 
