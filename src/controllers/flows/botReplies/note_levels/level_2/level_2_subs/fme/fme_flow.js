b3f9cc98633217c9b0d65d2abfc393929f1c385c
TriptoAfsin <afsintripto@gmail.com>
2021-06-21 18:27:16 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/fme/fme_flow.js
+++ src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/fme/fme_flow.js
@@ -15,13 +15,13 @@ let fmeFlow = [
     grroupedButtonBlockGen(`📌Full Hand Note(Nahid,AE-44, 2020)-`,
         [
             webBtnBlockGen("Part A", "https://drive.google.com/file/d/1tbFqkVmg9g-O-XGUeAaCM4bXF7NE6xHW/view"),
-            webBtnBlockGen("Part B", "https://drive.google.com/file/d/18W-ask6cB34n6hI2BD7olZ_i-acM8Jp4/view")
+            webBtnBlockGen("Part B", "https://drive.google.com/file/d/1vSKjJ5FtAsvcv-JBAJ0F9gz_XWg_4Old/view")
         ]
     ),
     grroupedButtonBlockGen(`📌Full Hand Note(Akib,AE-44,2020)-`,
         [
             webBtnBlockGen("Part A", "https://drive.google.com/file/d/1-4SddFXr6M6UlaMNplsNQElkaEdjwVaJ/view"),
-            webBtnBlockGen("Part B", "https://drive.google.com/file/d/1Gz7ihBrmSQSlirefPZ2aQVgX_CETC_AW/view?usp=sharing"),
+            webBtnBlockGen("Part B", "https://drive.google.com/file/d/18W-ask6cB34n6hI2BD7olZ_i-acM8Jp4/view"),
         ]
     ),
     grroupedButtonBlockGen(`🔰 Select Topics for FME - `,
