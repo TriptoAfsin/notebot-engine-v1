a0ef097627a49075ff3da6e05c91b3f21bc2c663
TriptoAfsin <afsintripto@gmail.com>
2021-07-09 14:52:54 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/marketing/topics/marketFunc.js
+++ src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/marketing/topics/marketFunc.js
@@ -1,16 +1,11 @@
+let textBlockGen = require("../../../../../../../genrators/textBlockGen");
+
+
+
 let marketFunc = [
-    {
-        "text": `🔷Functions of Marketing(Munir Sir) -  
-        https://drive.google.com/file/d/1PZ_U11LEbf7SLnucMNwRfQrOyXNPMoMW/view?usp=sharing`
-    },
-    {
-        "text": `🔷Hand Note(Tripto, 2019)-
-        https://drive.google.com/file/d/1utnWOITqwV4ZuCdvM0XtjyP2O33ZH0gA/view?usp=sharing`
-    },
-    {
-        "text": `🔷Hand Note(Akib,2019)-
-        https://drive.google.com/file/d/1I2fCTn1b-9VQ-XanSar3EHM11Zng8_vU/view`
-    }
+    textBlockGen(`🔷 Functions of Marketing(Munir Sir) -  \n\nhttps://drive.google.com/file/d/1PZ_U11LEbf7SLnucMNwRfQrOyXNPMoMW/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Tripto, 2019)-\n\nhttps://drive.google.com/file/d/1utnWOITqwV4ZuCdvM0XtjyP2O33ZH0gA/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Akib,2019)-\n\nhttps://drive.google.com/file/d/1MxCtOOFQIN2y6DVsGGXswHPAcWZUXPIU/view`),
 ]
 
 
