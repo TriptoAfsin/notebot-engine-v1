dea7c4a973b3b05f576ae9828400be9fa5f5b648
TriptoAfsin <afsintripto@gmail.com>
2021-08-17 20:14:04 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/acm/topics/acmAccountingAction.js
+++ src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/acm/topics/acmAccountingAction.js
@@ -4,6 +4,7 @@ let textBlockGen = require("../../../../../../../genrators/textBlockGen");
 let acmAccountingAction = [
     textBlockGen(`🔷 Slide(Emu Mam,2021)-\n\nhttps://drive.google.com/file/d/1VeXzJ8XdJLiHhlKaVRkLlbycuWJ6nlkh/view?usp=sharing`),
     textBlockGen(`🔷 Slide Exercise(Emu Mam,2021)-\n\nhttps://drive.google.com/file/d/1bSBjeoI3eV3aOQB0UGH4eSNwlKCVHuJe/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Akib, AE-44, 2021)-\n\nhttps://drive.google.com/file/d/17au4HlmDkgeKmRkxk9GDBI4mi_U6eRQZ/view`),
     textBlockGen(`🔷 Hand Note(Nahid, AE-44, 2020)-\n\nhttps://drive.google.com/file/d/1Pc98TU9S-ZFRJAvPeVSVyUqgHtZh9cD8/view?usp=sharing`),
 ]
 
--- src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/acm/topics/acmRecordingProcess.js
+++ src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/acm/topics/acmRecordingProcess.js
@@ -3,6 +3,7 @@ let textBlockGen = require("../../../../../../../genrators/textBlockGen");
 
 let acmRecordingProcess = [
     textBlockGen(`🔷 Slide (Ayesha mam, 2020)-\n\nhttps://drive.google.com/file/d/15LyVZgn5ultmTxSKNS_bCiOwtF7UU6QP/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Akib, AE-44, 2021)-\n\nhttps://drive.google.com/file/d/17b3VY0n4njbIztnZBsi1r2qgC8ZYvJ3n/view`),
     textBlockGen(`🔷 Math (Journal, Ledger, Trial Balance,(Nahid, AE-44)) - \n\nhttps://drive.google.com/file/d/17n9oZydoykmpUe1Ps-o0NB3ChLQcgjT0/view?usp=sharing`),
 ]
 
