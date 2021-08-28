8f3a26ce1cb15d659886e5520502b77746f84e73
TriptoAfsin <afsintripto@gmail.com>
2021-08-28 21:54:41 +0600

    ++

--- src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/fsd/topics/fsdIntro.js
+++ src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/fsd/topics/fsdIntro.js
@@ -1,8 +1,8 @@
+let textBlockGen = require("../../../../../../../genrators/textBlockGen");
+
 let fsdIntro = [
-    {
-        "text": `🔷Hand Note(Nahid AE-44, 2020)-
-        https://drive.google.com/file/d/16ZqgsgwV7-GOzlm0dbfJcdZM57qD8isv/view?usp=sharing`
-    }
+    textBlockGen(`🔷 Hand Note(Akib, AE-44, 2021)-\n\nhttps://drive.google.com/file/d/1MZA7grPpm_XhSvnqaeTI5AR0TZKfzJ6O/view`),
+    textBlockGen(`🔷 Hand Note(Nahid, AE-44, 2020)-\n\nhttps://drive.google.com/file/d/16ZqgsgwV7-GOzlm0dbfJcdZM57qD8isv/view?usp=sharing`)
 ]
 
 
--- src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/fsd/topics/fsdPlainWeave.js
+++ src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/fsd/topics/fsdPlainWeave.js
@@ -2,6 +2,7 @@ let textBlockGen = require("../../../../../../../genrators/textBlockGen");
 
 
 let fsdPlainWeave = [
+    textBlockGen(`🔷 Hand Note(AKib, AE-44, 2021)-\n\nhttps://drive.google.com/file/d/1MPnjR68qFVIE50_Igca1VtSBml4kwegr/view`),
     textBlockGen(`🔷 Hand Note(Nahid, AE-44, 2020)-\n\nhttps://drive.google.com/file/d/1A1Kxea4UfrZCxSpkSClR-3rcZZSd3zFW/view?usp=sharing`),
     textBlockGen(`🔷 Sheet(Mamun Sir, 2021)-\n\nhttps://drive.google.com/file/d/1-n5SL4HBgmkdBAZ8A9oIqHS5-02QqOED/view?usp=sharing`),
 ]
