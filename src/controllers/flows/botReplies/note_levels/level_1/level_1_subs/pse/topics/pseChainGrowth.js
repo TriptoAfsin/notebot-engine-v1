c8f8779750fdcf65bfd80c0c31223440a9edd47f
TriptoAfsin <afsintripto@gmail.com>
2021-09-10 21:24:40 +0600

    pse chain growth++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/pse/topics/pseChainGrowth.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/pse/topics/pseChainGrowth.js
@@ -1,8 +1,9 @@
-let textBlockGen = require("../../../../../../../genrators/textBlockGen");
+let textBlockGen = require("simple-messenger-blocks/textBlockGen");
 
 let pseChain = [
     textBlockGen(`🔷 Hand Note(Akib, 2018) - \n\nhttps://drive.google.com/file/d/1D_zlz6WTOIV4-wyZuL-DChihdiF-x6ft/view?usp=drivesdk`),
     textBlockGen(`🔷 Hand Note(Urmi,TEM-45, 2021) - \n\nhttps://drive.google.com/file/d/1RqlvU6b-zczdNz4f6tq6OYu26V10o8th/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Nafis,IPE-46, 2021) - \n\nhttps://drive.google.com/file/d/1KbIqYYPwl48U3lsJTVCWlGeW9qV8SI7P/view?usp=sharing`),
     textBlockGen(`🔷 Book Scanned (Mamun Sir)-\n\nhttps://drive.google.com/file/d/1-mo3uddku00VBop3idfcNzYXNviNtt0d/view?usp=drivesdk`),
     textBlockGen(`🔷 Presentation (Arzon, Al-Islam, Aurnob) - \n\nhttps://docs.google.com/presentation/d/1Ank7ZLF-JLLbaxJS-PPmFPTGaeC4MEEICPEDjuF1JUc/edit?usp=sharing`),
 ]
