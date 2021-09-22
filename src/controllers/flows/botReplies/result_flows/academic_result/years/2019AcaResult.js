07068cd26947c7fff8599afcf5e439a100dbeacf
TriptoAfsin <afsintripto@gmail.com>
2021-09-22 20:33:54 +0600

    result 2019 ++

--- src/controllers/flows/botReplies/result_flows/academic_result/years/2019AcaResult.js
+++ src/controllers/flows/botReplies/result_flows/academic_result/years/2019AcaResult.js
@@ -10,6 +10,7 @@ let acdemicRes2019 = [
         "🟡 Select Your Desired Result(New) -",
         [
             webBtnBlockGen("L 1,2(2021)", "https://drive.google.com/file/d/1orh48cQeiOc7OuzwVzm_oi9kPSRrAiLe/view"),
+            webBtnBlockGen("L 2,2(2021)", "https://www.butex.edu.bd/wp-content/uploads/2021/09/Result-of-B.Sc_.-in-Textile-Engineering-L-2-T-2-Examination-2019.pdf"),
         ]
     ),
     grroupedButtonBlockGen(
