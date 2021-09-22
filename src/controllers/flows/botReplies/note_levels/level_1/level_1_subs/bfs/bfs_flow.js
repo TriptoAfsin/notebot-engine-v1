15f7b82a54517ec8f1297f19ac5f00c8857cd987
TriptoAfsin <afsintripto@gmail.com>
2021-09-22 20:40:55 +0600

    fixed bfs words overlap and bfs sheets++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/bfs/bfs_flow.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/bfs/bfs_flow.js
@@ -1,20 +1,19 @@
+let grroupedButtonBlockGen = require("simple-messenger-blocks/groupedBtnBlockGen");
+let webBtnBlockGen = require("simple-messenger-blocks/webBtnGen");
+let payloadBtnGen = require("simple-messenger-blocks/payloadBtnGen");
+let cardGenerator = require("simple-messenger-blocks/cardGenerator");
+let bondiFlow = require("../../../../sponsoredFlows/bondiCard");
+
+
+
 let bfs_flow = [
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🟩 Select Topic for BFS -",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Lecture 3",
-                        "payload": "bfs_lec3_flow"
-                    }
-                ],
-            },
-        },
-    }
+    grroupedButtonBlockGen(
+        "🔰 Select Topic for BFS -",
+        [
+            webBtnBlockGen("All Slide(2021)", "https://drive.google.com/drive/folders/132FN_kPTdwiCn4HHspHQ7xBREf35JP9P?usp=sharing"),
+            payloadBtnGen("Lecture 3", "bfs_lec3_flow")
+        ]
+    ),
 ]
 
 
--- src/controllers/keywords/gfBfWords.js
+++ src/controllers/keywords/gfBfWords.js
@@ -2,7 +2,6 @@ const gfBFWords = [
     "girlfriend",
     "boyfriend",
     "gf",
-    "bf",
     "গার্লফ্রেন্ড",
     "বয়ফ্রেন্ড",
     "জামাই",
