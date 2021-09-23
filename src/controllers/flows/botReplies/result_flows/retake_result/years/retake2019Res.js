93c9b003dd5f7e79f7ae1622d1a76f894ad73dee
TriptoAfsin <afsintripto@gmail.com>
2021-09-23 18:08:28 +0600

    retae res++ & retake res refactoring

--- src/controllers/flows/botReplies/result_flows/retake_result/years/retake2019Res.js
+++ src/controllers/flows/botReplies/result_flows/retake_result/years/retake2019Res.js
@@ -1,46 +1,25 @@
+let grroupedButtonBlockGen = require("simple-messenger-blocks/groupedBtnBlockGen");
+let webBtnBlockGen = require("simple-messenger-blocks/webBtnGen");
+let payloadBtnGen = require("simple-messenger-blocks/payloadBtnGen");
+let cardGenerator = require("simple-messenger-blocks/cardGenerator");
+
+
 let retakeRes2019 = [
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🟡 Select Your Desired Result - ",
-                "buttons": [
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/17ZXfHpyx4OBXSaEJUc4aWM5scTxvyJGU/edit",
-                        "title": "1,1 (New)",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/13S4x2gIa1qSWcp4VslSYroPftjlogn7H/view",
-                        "title": "2,1(New)",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1Fi8P78FO-spX77Gd5RPv7gdinEgOe_CF/view",
-                        "title": "3,1(New)",
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🟡 Select Your Desired Result - ",
-                "buttons": [
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1NGhfVSSI_tH7psk1LkrjLQRepqi2E4GW/view",
-                        "title": "4,1(New)",
-                    },
-                ],
-            },
-        },
-    },
+    grroupedButtonBlockGen(
+        "🟡 Select Your Desired Result -",
+        [
+            webBtnBlockGen("4,1", "https://drive.google.com/file/d/1NGhfVSSI_tH7psk1LkrjLQRepqi2E4GW/view"),
+            webBtnBlockGen("2,2(New)", "https://www.butex.edu.bd/wp-content/uploads/2021/09/Result-of-Retake-Subjects-B.Sc_.-in-Textile-Engineering-L-2-T-2-Examination-2019.pdf"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🟡 Select Your Desired Result -",
+        [
+            webBtnBlockGen("1,1", "https://drive.google.com/file/d/17ZXfHpyx4OBXSaEJUc4aWM5scTxvyJGU/edit"),
+            webBtnBlockGen("2,1", "https://drive.google.com/file/d/13S4x2gIa1qSWcp4VslSYroPftjlogn7H/view"),
+            webBtnBlockGen("3,1", "https://drive.google.com/file/d/1Fi8P78FO-spX77Gd5RPv7gdinEgOe_CF/view"),
+        ]
+    ),
 ]
 
 
