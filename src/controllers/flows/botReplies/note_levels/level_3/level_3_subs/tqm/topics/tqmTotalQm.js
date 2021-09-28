e9d4153ca9f4df253b3776c5af8a08996098b3d4
TriptoAfsin <afsintripto@gmail.com>
2021-09-28 18:12:58 +0600

    tqm total qm++

--- src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/tqm/topics/tqmTotalQm.js
+++ src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/tqm/topics/tqmTotalQm.js
@@ -3,6 +3,7 @@ let textBlockGen = require("../../../../../../../genrators/textBlockGen");
 let tqmTotalQM = [
     textBlockGen(`🔷 Hand Note(Nahid AE-44, 2020)-\n\nhttps://drive.google.com/file/d/1AwayTmBBi7HWMnEKN_4bnzm5i7CoHd2I/view?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Tripto, IPE-44, 2021)-\n\nhttps://drive.google.com/file/d/1MY2dsFmebD5glejrhtZ3celHXkX4paJK/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Akib, AE-44, 2021)-\n\nhttps://drive.google.com/file/d/1CWeyNK63vgVQYwsE6Gtu7_I9GnxfqIPE/view`),
     textBlockGen(`🔷 Ahsan Sir Sheet - \n\nhttps://drive.google.com/file/d/1xYAl6OAgJj7W7Dhf0IsV2hMkS5zOlH28/view?usp=sharing`),
 ]
 
