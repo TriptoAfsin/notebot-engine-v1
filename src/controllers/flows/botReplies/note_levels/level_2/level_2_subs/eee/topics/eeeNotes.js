ebba8df49941e6ad4204336127243c3610b958a9
TriptoAfsin <afsintripto@gmail.com>
2021-10-05 11:19:46 +0600

    eee notes refactoring

--- src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/eee/topics/eeeNotes.js
+++ src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/eee/topics/eeeNotes.js
@@ -1,16 +1,10 @@
+let textBlockGen = require('simple-messenger-blocks/textBlockGen');
+
+
 let eeeNotes = [
-    {
-        "text": `🔷 Circuit(Tania, 2018) - 
-        https://drive.google.com/file/d/15yQ7FspBxzzMShEGxx-o9DLh-u7MeImy/view?usp=sharing`
-    },
-    {
-        "text": `🔷  Fundamentals of EEE(Nadim,2018) -  
-        https://drive.google.com/file/d/1bAE70RQLXv6811t2XB0CASqdlCCzVSw-/view?usp=sharing`
-    },
-    {
-        "text": `🔷 Part B Handnote(Arma)-
-        https://drive.google.com/file/d/1YtwUbhp--3zPQxADo1kJcccZXbIKBmVw/view?usp=sharing`
-    }
+    textBlockGen(`🔷 Circuit(Tania, 2018) - \n\nhttps://drive.google.com/file/d/15yQ7FspBxzzMShEGxx-o9DLh-u7MeImy/view?usp=sharing`),
+    textBlockGen(`🔷  Fundamentals of EEE(Nadim,2018) -  \n\nhttps://drive.google.com/file/d/1bAE70RQLXv6811t2XB0CASqdlCCzVSw-/view?usp=sharing`),
+    textBlockGen(`🔷 Part B Handnote(Arma)- \n\nhttps://drive.google.com/file/d/1YtwUbhp--3zPQxADo1kJcccZXbIKBmVw/view?usp=sharing`),
 ]
 
 
