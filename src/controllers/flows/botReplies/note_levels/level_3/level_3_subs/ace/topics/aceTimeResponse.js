c3da82564795841df205cc75b5db712a4c2de56f
TriptoAfsin <afsintripto@gmail.com>
2021-10-15 14:29:00 +0600

    ace time res ++

--- src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/ace/topics/aceTimeResponse.js
+++ src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/ace/topics/aceTimeResponse.js
@@ -1,6 +1,7 @@
 let textBlockGen = require("../../../../../../../genrators/textBlockGen");
 
 let aceTimeresponse = [
+    textBlockGen(`🔷 Hand Note(Akib AE-44, 2021)(New)-\n\nhttps://drive.google.com/file/d/1I61khqdY3V0Oz4kE2msGtT4CVnu-GJEp/view`),
     textBlockGen(`🔷 Slide(Janan Mam, 2021)-\n\nhttps://drive.google.com/file/d/1PJze_KO8k23f3wOWADEfCbqC6pJL9FCS/view?usp=sharing`),
     textBlockGen(`🔷 Math (Janan Mam, 2021)-\n\nhttps://drive.google.com/file/d/1el8NlR5wrem-nB8JwpfFfaS-9iqyE0Ao/view?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Nahid AE-44, 2021)(New)-\n\nhttps://drive.google.com/file/d/1wyXd6OLA7Vfy2Y5gEe0XUorcoMNKvwXU/view?usp=sharing`),
