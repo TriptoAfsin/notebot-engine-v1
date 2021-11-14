d9370e3d7eafebb9e1390b69c4a4b9252a780a8f
TriptoAfsin <afsintripto@gmail.com>
2021-11-14 18:57:51 +0600

    fsd twill++

--- src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/fsd/topics/fsdTwill.js
+++ src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/fsd/topics/fsdTwill.js
@@ -3,6 +3,7 @@ let textBlockGen = require("../../../../../../../genrators/textBlockGen");
 
 let fsdTwill = [
     textBlockGen(`🔷 Sheet(Mamun Sir, 2021)-\n\nhttps://drive.google.com/file/d/1-n5SL4HBgmkdBAZ8A9oIqHS5-02QqOED/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Akib,AE-44,2021)-\n\nhttps://drive.google.com/file/d/14xc7kCvFz25DK9Nd5w-dGk7isM60EaeR/view`)
 ]
 
 
