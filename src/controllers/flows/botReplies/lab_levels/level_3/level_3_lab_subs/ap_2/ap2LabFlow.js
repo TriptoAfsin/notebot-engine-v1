4c9459cc0ba9608f8174fb72511942144077b794
TriptoAfsin <afsintripto@gmail.com>
2021-11-25 00:06:28 +0600

    ap2 lab++

--- src/controllers/flows/botReplies/lab_levels/level_3/level_3_lab_subs/ap_2/ap2LabFlow.js
+++ src/controllers/flows/botReplies/lab_levels/level_3/level_3_lab_subs/ap_2/ap2LabFlow.js
@@ -1,20 +1,16 @@
+let grroupedButtonBlockGen = require('simple-messenger-blocks/groupedBtnBlockGen');
+let webBtnBlockGen = require('simple-messenger-blocks/webBtnGen');
+let payloadBtnGen = require('simple-messenger-blocks/payloadBtnGen');
+
+
 let ap2_lab_flow = [
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Experiment for AP-II - ",
-                "buttons": [
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1DPt9c0tVcXOJSxpgx9gROhkzxvUNfvEe/view",
-                        "title": "All Report(Nadeem)",
-                    }
-                ],
-            },
-        },
-    }
+    grroupedButtonBlockGen(
+        "📌 All Reports - ",
+        [
+            webBtnBlockGen("Naimur - 2021", "https://drive.google.com/file/d/1cTWxp54APJ0J1ElwSCzd5z-OggQ3W5qB/view?usp=sharing"),
+            webBtnBlockGen("Nadeem - 2022", "https://drive.google.com/file/d/1DPt9c0tVcXOJSxpgx9gROhkzxvUNfvEe/view"),
+        ]
+    ),
 ]
 
 
