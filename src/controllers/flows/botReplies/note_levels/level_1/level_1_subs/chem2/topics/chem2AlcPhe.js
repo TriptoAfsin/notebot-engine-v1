4bfeb0aa7ec89006d4e31eff139f62fac4f74322
TriptoAfsin <afsintripto@gmail.com>
2021-12-27 23:02:25 +0600

    chem2: alc++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem2/topics/chem2AlcPhe.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/chem2/topics/chem2AlcPhe.js
@@ -1,11 +1,13 @@
 let textBlockGen = require('simple-messenger-blocks/textBlockGen');
 
 let chem2AlcPhe = [
+    textBlockGen(`🔷 Hand Note(Nihat, YE-46, 2201)-\n\nhttps://drive.google.com/file/d/1o60izC6HZGLxCrzthLTUSr6zk0Y6NbRm/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Nafis, IPE-46, 2201)-\n\nhttps://drive.google.com/file/d/1P2YO_gzbjGW1lBPDJ9SlcidGeTtm0PRc/view?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Tripto, 2018, Based on Brishty Mam's Lecture)-\n\nhttps://drive.google.com/file/d/1a1AOe9jGG40PiHZMe9pvaeHWjX3-xzsd/view?usp=sharing`),
     textBlockGen(`🔷 QB Solve(Nadia, 2020)-\n\nhttps://drive.google.com/file/d/1BNb6bRiGcHDzdKST477N7V56AGL6f72m/view?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Sabiha, 2019)-\n\nhttps://drive.google.com/file/d/1kxQdAQj64bnEYP9xkzcxAZslMj0K-I-R/view?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Tanim, 2019)-\n\nhttps://drive.google.com/file/d/139ZXT9zsmDQRLakKCGvYYwMhD2TnVPV2/view?usp=sharing`),
-    textBlockGen(`🔷 Hand Note(Shatil) - \n\nhttps://drive.google.com/file/d/1dSVR7WQjh5aY6CxRJaWCfRfURNTUDE4q/view?usp=sharing`),
+    // textBlockGen(`🔷 Hand Note(Shatil) - \n\nhttps://drive.google.com/file/d/1dSVR7WQjh5aY6CxRJaWCfRfURNTUDE4q/view?usp=sharing`),
     textBlockGen(`🔷 Class Lecture(Amjad Sir) - \n\nhttps://drive.google.com/file/d/19B8EwWp9h7ErW5tFYDBU1BuWktxhHG1a/view?usp=sharing`),
     textBlockGen(`🔷 Alcohols (Book Scanned) - \n\nhttps://drive.google.com/file/d/0B0FdOS_BOGE3Y0E2Y1NCXzRFeEk/view?usp=sharing`),
 ]
