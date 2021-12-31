670c3d08c6cd7fd776c2a16d7ddf80c7ec381e2e
TriptoAfsin <afsintripto@gmail.com>
2021-12-31 20:16:45 +0600

    ace++

--- src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/ace/topics/aceIntroControlSys.js
+++ src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/ace/topics/aceIntroControlSys.js
@@ -1,6 +1,7 @@
 let textBlockGen = require("../../../../../../../genrators/textBlockGen");
 
 let aceIntro = [
+    textBlockGen(`🔷 Hand Note(Akib, AE-44, 2021)-\n\nhttps://drive.google.com/file/d/11uMctoi5QdZ8kmDeNMbyJhqROHhChrw6/view`),
     textBlockGen(`🔷 Hand Note(Nahid AE-44, 2020)-\n\nhttps://drive.google.com/file/d/1_D4ag3ui7S5QsyiluIgDe9ym2eh0HaCT/view?usp=sharing`),
     textBlockGen(`🔷 Janan Madam Sheet(2020)-\n\nhttps://drive.google.com/file/d/1gT2g-rZT9holW_mfwCXkHB0MhKAuf9Va/view?usp=sharing`), 
     textBlockGen(`🔷 Ahsan Sir Sheet(2020)-\n\nhttps://drive.google.com/file/d/1Jo6VCJfP4LmABlnRwHZCSUcougmLyNdU/view?usp=sharing`),   
--- src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/ace/topics/aceModleingFreq.js
+++ src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/ace/topics/aceModleingFreq.js
@@ -1,10 +1,9 @@
+let textBlockGen = require('simple-messenger-blocks/textBlockGen');
+
 let aceModleing = [
-    {
-        "text": `🔷 Sheet(Janan Mam, 2020) -\n\nhttps://drive.google.com/file/d/1OP1Ht6tUzdZukuOo1o93wSyodH2qDl63/view?usp=sharing`
-    },
-    {
-        "text": `🔷 Hand Note(Nahid AE-44, 2020)-\n\nhttps://drive.google.com/file/d/1tGylfX7YeZkJCGeh0jLPzbpnWSxcOhNE/view?usp=sharing`
-    }
+    textBlockGen(`🔷 Sheet(Janan Mam, 2020) -\n\nhttps://drive.google.com/file/d/1OP1Ht6tUzdZukuOo1o93wSyodH2qDl63/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Akib AE-44, 2021)-\n\nhttps://drive.google.com/file/d/11__CTiyHdappHwcl2mBEuBueiLUTSgQQ/view`),
+    textBlockGen(`🔷 Hand Note(Nahid AE-44, 2020)-\n\nhttps://drive.google.com/file/d/1tGylfX7YeZkJCGeh0jLPzbpnWSxcOhNE/view?usp=sharing`)
 ]
 
 
