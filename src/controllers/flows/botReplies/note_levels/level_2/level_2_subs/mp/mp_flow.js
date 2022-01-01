c3298f254c13a7b3890db331af912e1ee0bd827b
TriptoAfsin <afsintripto@gmail.com>
2022-01-01 19:25:09 +0600

    mp++

--- src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/mp/mp_flow.js
+++ src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/mp/mp_flow.js
@@ -1,155 +1,63 @@
+let grroupedButtonBlockGen = require('simple-messenger-blocks/groupedBtnBlockGen');
+let webBtnBlockGen = require('simple-messenger-blocks/webBtnGen');
+let payloadBtnGen = require('simple-messenger-blocks/payloadBtnGen');
+let bondiFlow = require("../../../../sponsoredFlows/bondiCard");
+
+
 let mpFlow = [
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for MP - ",
-                "buttons": [
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/13VOsvTc6rk28xlxCrpklgKrCoSA1INeA/view",
-                        "title": "Production Process",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1DmVxIsqFiDkbfayM4lHgsPiFjJbgpXXA/view",
-                        "title": "MC Opera.. & MC Tool",
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Defects",
-                        "payload": "mp_defects_flow"
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for MP - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Lathe M/C(New)",
-                        "payload": "mp_latheMc_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Milling M/C",
-                        "payload": "mp_millingMc_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Casting Video",
-                        "payload": "mp_castingVideo_flow"
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for MP - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Welding",
-                        "payload": "mp_welding_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Hot & Cold Working",
-                        "payload": "mp_hotCold_flow"
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1PX02MP7cFK6KJhjwqIKouiBfYAuRbd-l/view",
-                        "title": "Forging, Ring Roll.",
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for MP - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Ceramics",
-                        "payload": "mp_ceramics_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Plastic",
-                        "payload": "mp_plastic_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Slideways",
-                        "payload": "mp_slideways_flow"
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for WP-I - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Shaper MC",
-                        "payload": "mp_shaperMc_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "EDM M/C",
-                        "payload": "mp_edm_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Non-Conventional M/C",
-                        "payload": "mp_non_conventional_flow"
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for MP - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Machining Economics",
-                        "payload": "mp_mcEconomics_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Casting",
-                        "payload": "mp_casting_flow"
-                    }
-                ],
-            },
-        },
-    }
+    grroupedButtonBlockGen(
+        "📌 Select Topics for MP -",
+        [
+            webBtnBlockGen("Questions", "https://drive.google.com/file/d/1L4qjNlHTAu8aWdUzVrgz1s_Vhnmvqz0M/view?usp=sharing"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for MP -",
+        [
+            webBtnBlockGen("Production Process", "https://drive.google.com/file/d/13VOsvTc6rk28xlxCrpklgKrCoSA1INeA/view"),
+            webBtnBlockGen("MC Opera.. & MC Tool", "https://drive.google.com/file/d/1DmVxIsqFiDkbfayM4lHgsPiFjJbgpXXA/view"),
+            payloadBtnGen("Defects", "mp_defects_flow"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for MP -",
+        [
+            payloadBtnGen("Lathe M/C(New)", "mp_latheMc_flow"),
+            payloadBtnGen("Milling M/C", "mp_millingMc_flow"),
+            payloadBtnGen("Casting Video", "mp_castingVideo_flow"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for MP -",
+        [
+            payloadBtnGen("Welding", "mp_welding_flow"),
+            payloadBtnGen("Hot & Cold Working", "mp_hotCold_flow"),
+            webBtnBlockGen("Forging, Ring Roll.", "https://drive.google.com/file/d/1PX02MP7cFK6KJhjwqIKouiBfYAuRbd-l/view"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for MP -",
+        [
+            payloadBtnGen("Ceramics", "mp_ceramics_flow"),
+            payloadBtnGen("Plastic", "mp_plastic_flow"),
+            payloadBtnGen("Slideways", "mp_slideways_flow"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for MP -",
+        [
+            payloadBtnGen("Shaper MC", "mp_shaperMc_flow"),
+            payloadBtnGen("EDM M/C", "mp_edm_flow"),
+            payloadBtnGen("Non-Conventional M/C", "mp_non_conventional_flow"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for MP -",
+        [
+            payloadBtnGen("Machining Economics", "mp_mcEconomics_flow"),
+            payloadBtnGen("Casting", "mp_casting_flow")
+        ]
+    )
 ]
 
 
