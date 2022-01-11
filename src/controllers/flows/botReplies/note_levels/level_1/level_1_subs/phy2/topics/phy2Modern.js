06273151ae40b9130d2a187b53fa6bb1d3f4e7c6
TriptoAfsin <afsintripto@gmail.com>
2022-01-11 14:45:22 +0600

    phy2++

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/phy2/topics/phy2Modern.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/phy2/topics/phy2Modern.js
@@ -3,6 +3,7 @@ let textBlockGen = require("../../../../../../../genrators/textBlockGen");
 
 let phy2Modern = [
     textBlockGen(`🔷 Hand Note(Tripto, 2019)-\n\nhttps://drive.google.com/file/d/1QAyuTIO_DjhCqTE-NvSCDNyVwaCu73WO/view?usp=sharing`),
+    textBlockGen(`🔷 Hand Note(Farhan,TEM-46,2021)-\n\nhttps://drive.google.com/file/d/16-z9xcQqg8nwWSXvRQrHyIJbwNk1D2fJ/view?usp=sharing`),
     textBlockGen(`🔷 Book Scanned -\n\nhttps://drive.google.com/file/d/1wVHVFo0VLAXIb4H3g58OAXezPtObdNaF/view?usp=sharing`),
     textBlockGen(`🔷 Slide (Hasina Mam, 2019)-\n\nhttps://drive.google.com/drive/folders/1ofFj1ZztB2J8gjs-Xn4fo2rEnwhjDdEy?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Nahid, 2019)-\n\nhttps://drive.google.com/file/d/10y9MUXjO2JVwmBsIsB6cWgZbOeQT_84b/view?usp=sharing`),
