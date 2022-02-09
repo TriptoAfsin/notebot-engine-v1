17c695261fce4aac81f56df40e239c5a59724ad7
TriptoAfsin <afsintripto@gmail.com>
2022-02-09 19:20:49 +0600

    routine ++ & env.example++

--- src/controllers/flows/botReplies/routineFlow.js
+++ src/controllers/flows/botReplies/routineFlow.js
@@ -7,8 +7,8 @@ let routines = [
     grroupedButtonBlockGen(
         "🔴 Latest Routines -",
         [
-            webBtnBlockGen("Exam L2,1(2022)", "https://drive.google.com/file/d/1hlagbuwTw7JgHiLaix0JCTjIAPMChwhJ/view?usp=sharing"),
-            webBtnBlockGen("Exam L3,1(2022)", "https://drive.google.com/file/d/119kgW-OIo6AY5fBhO4z7vr3ItKnYu610/view?usp=sharing"),
+            webBtnBlockGen("Exam L2,1(2022)", "https://www.butex.edu.bd/exam-schedule-b-sc-in-textile-engineering-l-2-t-1-exam-2020/"),
+            webBtnBlockGen("Exam L3,1(2022)", "https://www.butex.edu.bd/exam-schedule-b-sc-in-textile-engineering-l-3-t-1-exam-2020/"),
             webBtnBlockGen("Exam L4,1(2022)", "https://drive.google.com/file/d/1dCc1T1dXd_ZtKYd4e60uhz99T2P9bck7/view?usp=sharing"),
         ]
     ),
