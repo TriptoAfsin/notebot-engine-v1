f6baab7032e5f75724ede7516935234a05613879
TriptoAfsin <afsintripto@gmail.com>
2022-02-09 19:31:12 +0600

    result ++

--- src/controllers/flows/botReplies/resultFlow.js
+++ src/controllers/flows/botReplies/resultFlow.js
@@ -8,9 +8,9 @@ let result = [
     grroupedButtonBlockGen(
         "🔴 Latest Results -",
         [
+            webBtnBlockGen("L1, T1(New)", "https://www.butex.edu.bd/wp-content/uploads/2022/02/Result-of-B.Sc_.-in-Textile-Engineering-L-1-T-1-Exam-2020.pdf"),
             webBtnBlockGen("Retake L1T2", "https://www.butex.edu.bd/result-of-retake-subjects-b-sc-in-textile-engineering-l-1-t-2-exam-2019/"),
-            webBtnBlockGen("L4T2(2021)", "https://www.butex.edu.bd/wp-content/uploads/2021/10/Result-of-B.Sc_.-in-Textile-Engineering-L-4-T-2-Dated-28102021.pdf"),
-            webBtnBlockGen("L3T2(2021)", "https://www.butex.edu.bd/wp-content/uploads/2021/09/Result-of-B.Sc_.-in-Textile-Engineering-L-3-T-2-Examination-2019.pdf"),
+            webBtnBlockGen("L4T2(2021)", "https://www.butex.edu.bd/wp-content/uploads/2021/10/Result-of-B.Sc_.-in-Textile-Engineering-L-4-T-2-Dated-28102021.pdf")
         ]
     ),
     grroupedButtonBlockGen(
