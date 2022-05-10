87fd98cb262977e3182c987a3619b4190955c3d7
TriptoAfsin <AfsinTripto@gmail.com>
2022-05-10 19:16:18 +0600

    im ++

--- src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/im/im_flow.js
+++ src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/im/im_flow.js
@@ -1,124 +1,53 @@
+let grroupedButtonBlockGen = require('simple-messenger-blocks/groupedBtnBlockGen');
+let webBtnBlockGen = require('simple-messenger-blocks/webBtnGen');
+let payloadBtnGen = require('simple-messenger-blocks/payloadBtnGen');
+
+
 let imFlow = [
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for IM - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Lectures",
-                        "payload": "im_lecture_flow"
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for IM - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Lec 1: Intro to IM",
-                        "payload": "im_lec1_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Lec 2: Manegerial ..",
-                        "payload": "im_lec2_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Lec 3: Environment.",
-                        "payload": "im_lec3_flow"
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for IM - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Lec 4: Motivating E",
-                        "payload": "im_lec4_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Market",
-                        "payload": "im_market_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Market Mix",
-                        "payload": "im_marketMix_flow"
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for IM - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Project & Feasi..",
-                        "payload": "im_projectFeasible_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Nature & Scope of B.",
-                        "payload": "im_natureScope_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Management & Func",
-                        "payload": "im_managementFunc_flow"
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Select Topics for IM - ",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Industrial Manag.",
-                        "payload": "im_indusManage_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Technology Manage",
-                        "payload": "im_techManage_flow"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Production Mange.",
-                        "payload": "im_prodMange_flow"
-                    }
-                ],
-            },
-        },
-    }
+    grroupedButtonBlockGen(
+        "📌 Select Topics for IM(New) -",
+        [
+            webBtnBlockGen("Marketing Basics", "https://drive.google.com/file/d/1-V7IyR_jCb7SoZ9XD46k5av4bJrZf0MC/view"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for IM -",
+        [
+            payloadBtnGen("Lectures", "im_lecture_flow"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for IM -",
+        [
+            payloadBtnGen("Lec 1: Intro to IM", "im_lec1_flow"),
+            payloadBtnGen("Lec 2: Manegerial ..", "im_lec2_flow"),
+            payloadBtnGen("Lec 3: Environment.", "im_lec3_flow"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for IM -",
+        [
+            payloadBtnGen("Lec 4: Motivating E", "im_lec4_flow"),
+            payloadBtnGen("Market", "im_market_flow"),
+            payloadBtnGen("Market Mix", "im_marketMix_flow"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for IM -",
+        [
+            payloadBtnGen("Project & Feasi..", "im_projectFeasible_flow"),
+            payloadBtnGen("Nature & Scope of B.", "im_natureScope_flow"),
+            payloadBtnGen("Management & Func", "im_managementFunc_flow"),
+        ]
+    ),
+    grroupedButtonBlockGen(
+        "🔰 Select Topics for IM -",
+        [
+            payloadBtnGen("Industrial Manag.", "im_indusManage_flow"),
+            payloadBtnGen("Technology Manage", "im_techManage_flow"),
+            payloadBtnGen("Production Mange.", "im_prodMange_flow"),
+        ]
+    ),
 ]
 
 
