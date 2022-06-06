4a17d30f1abd267bed7c2522f1ec4d3695f148ad
TriptoAfsin <AfsinTripto@gmail.com>
2022-06-07 00:42:21 +0600

    fm2++

--- src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/fm2/topics/fm2Books.js
+++ src/controllers/flows/botReplies/note_levels/level_3/level_3_subs/fm2/topics/fm2Books.js
@@ -1,20 +1,11 @@
+let textBlockGen = require('simple-messenger-blocks/textBlockGen');
+
 let fm2Books = [
-    {
-        "text": `🔷 Knitting Technology (Anbumani) - 
-        https://drive.google.com/file/d/13uJB4NfN49f2T441sAlaMN5HpkEqdgNx/view?usp=sharing`
-    },
-    {
-        "text": `🔷 Fundamentals & Advances in Knitting Technology - 
-        https://drive.google.com/file/d/1iNgDMm8eQ8jrBnahQw7Pwly7AfJDFgRg/view?usp=sharing`
-    },
-    {
-        "text": `🔷 Knitting Technology (David J Spencer) - 
-        https://drive.google.com/file/d/1UpbfevM_H9Ndf3s_jK1iRddwYgVT2kIg/view?usp=sharing`
-    },
-    {
-        "text": `🔷 Understanding Textiles For a Merchandiser - 
-        https://drive.google.com/file/d/1_yH8RnXyJ47luoHfFcKlCysCmjSgZakY/view?usp=sharing`
-    }
+    textBlockGen(`🔷 Knitting Technology (Anbumani) - \n\nhttps://drive.google.com/file/d/13uJB4NfN49f2T441sAlaMN5HpkEqdgNx/view?usp=sharing`),
+    textBlockGen(`🔷 Fundamentals & Advances in Knitting Technology - \n\nhttps://drive.google.com/file/d/1iNgDMm8eQ8jrBnahQw7Pwly7AfJDFgRg/view?usp=sharing`),
+    textBlockGen(`🔷 Knitting Technology (David J Spencer) - \n\nhttps://drive.google.com/file/d/1UpbfevM_H9Ndf3s_jK1iRddwYgVT2kIg/view?usp=sharing`),
+    textBlockGen(`🔷 Understanding Textiles For a Merchandiser - \n\nhttps://drive.google.com/file/d/1_yH8RnXyJ47luoHfFcKlCysCmjSgZakY/view?usp=sharing`),
+    textBlockGen(`🔷 Understanding Textiles For a Merchandiser(Colored, Monir, 2022)- \n\nhttps://drive.google.com/file/d/1TSfcyVzLUd6jrpFInqiRpuK9gWfTX_rS/view?usp=sharing`),
 ]
 
 
