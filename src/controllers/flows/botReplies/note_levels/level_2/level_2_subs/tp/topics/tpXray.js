87656921fa1f3824ba7a015e6f06b22fe3de53ba
TriptoAfsin <AfsinTripto@gmail.com>
2022-09-07 21:53:28 +0600

    tp, math1++

--- src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/tp/topics/tpXray.js
+++ src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/tp/topics/tpXray.js
@@ -2,6 +2,7 @@ let textBlockGen = require('simple-messenger-blocks/textBlockGen');
 
 let tpXray = [
     textBlockGen(`🔷 Hand Note(Akib ,2019)-\n\nhttps://drive.google.com/file/d/1Hud6GUxMbi0qFM19rS1PI3pkOobMXgCH/view`),
+    textBlockGen(`🔷 Hand Note(Urmi,YE-45,2022)-\n\nhttps://drive.google.com/file/d/1-uD9wm3qZz7G4iQBSN-qihilmdBpiTc8/view?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Nahid, 2019)-\n\nhttps://drive.google.com/file/d/1AU-JIZ3PURpUaBuf88jcw2hi8ePJsfPj/view?usp=sharing`)
 ]
 
