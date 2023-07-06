0bcf5f91157c2076dc42aab8f8d803c7bbc41d75
TriptoAfsin <AfsinTripto@gmail.com>
2023-07-06 15:28:45 +0600

    update pse intro book scanned url

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/pse/topics/pseIntro.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/pse/topics/pseIntro.js
@@ -5,7 +5,7 @@ let pseIntro = [
     textBlockGen(`🔷 Hand Note(Tripto, 2018) - \n\nhttps://drive.google.com/file/d/1yJC2JxTBW01TPQByVaswyvCR_pMJpprx/view?usp=sharing`),
     textBlockGen(`🔷 Hand Note(Akib, 2018) - \n\nhttps://drive.google.com/file/d/11uivp4QtbfPS-hvPBwIF3QpVy7ViN38u/view?usp=drivesdk`),
     textBlockGen(`🔷 Hand Note(Nafis,IPE-46,2021) - \n\nhttps://drive.google.com/file/d/1mO6eA2AVsB1J_WgdbIeeLm_Fk_KSnHC2/view?usp=sharing`),
-    textBlockGen(`🔷 Book Scanned - \n\nhttps://drive.google.com/file/d/0B0FdOS_BOGE3THdvNzA0YkJlOUU/view?usp=sharing`),
+    textBlockGen(`🔷 Book Scanned - \n\nhttps://drive.google.com/file/d/1DEEuCZedmAoPaYKbrtELje_ve2RZ2e4e/view?usp=drivesdk`),
 ]
 
 
