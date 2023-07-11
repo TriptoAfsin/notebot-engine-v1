cf570ceea81cfbeee61cf4ff09fa7b6f0c69b082
TriptoAfsin <afsintripto@gmail.com>
2023-07-11 13:11:06 +0600

    manza fix 4

--- src/controllers/flows/botReplies/manzaFlow/manzaGetStarted.js
+++ src/controllers/flows/botReplies/manzaFlow/manzaGetStarted.js
@@ -5,24 +5,26 @@ const webBtnBlockGen = require("../../../genrators/webBtnBlockGen");
 const payloadBtnBlockGen = require("../../../genrators/payloadBtnGen");
 const groupedBtnBlockGen = require("../../../genrators/grroupedButtonBlockGen");
 
-let manzaGetStarted = [
-  textBlockGen(`Welcome to ManzaBot 😃 !`),
-  "quick_replies": [{
-    "content_type": "text",
-    "title": "📦 Browse Catalogue",
-    "payload": "manza_catalogue"
-},
-{
-    "content_type": "text",
-    "title": "💌 Contact Us",
-    "payload": "manza_contact"
-},
-{
-    "content_type": "text",
-    "title": "👋 Help/ FAQ",
-    "payload": "manza_faq"
-},
+let manzaGetStarted = {
+
+    "text": `Welcome to ManzaBot 😃 !`,
+     "quick_replies": [{
+       "content_type": "text",
+       "title": "📦 Browse Catalogue",
+       "payload": "manza_catalogue"
+   },
+   {
+       "content_type": "text",
+       "title": "💌 Contact Us",
+       "payload": "manza_contact"
+   },
+   {
+       "content_type": "text",
+       "title": "👋 Help/ FAQ",
+       "payload": "manza_faq"
+   },
 ]
-];
+}
+;
 
 module.exports = manzaGetStarted;
