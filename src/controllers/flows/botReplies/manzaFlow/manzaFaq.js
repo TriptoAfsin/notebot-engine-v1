b936e7be7534165edf9a53193d0cc256d436a1d9
TriptoAfsin <afsintripto@gmail.com>
2023-07-11 14:11:36 +0600

    manza fix 7

--- src/controllers/flows/botReplies/manzaFlow/manzaFaq.js
+++ src/controllers/flows/botReplies/manzaFlow/manzaFaq.js
@@ -7,9 +7,7 @@ const groupedBtnBlockGen = require("../../../genrators/grroupedButtonBlockGen");
 
 let manzaFaq = [
   textBlockGen(
-    `1. What is WebManza?  \nWebManza is an eCommerce solution that offers custom drag-and-drop website design and development, eCommerce functionality, inventory management, shipping and logistics to build businesses and easily manage their online presence.\n\n
-    2. Do I need coding experience to use WebManza?  \n No.\n\n
-    3. How can I onboard with WebManza?\n With WebManza, you will have your site immediately! No delay, no waiting. Visit: www.webmanza.com/store-create and complete your WebManza subscription. You will instantly get access to your admin panel to start working on your website.`
+    `1. What is WebManza?  \n\nWebManza is an eCommerce solution that offers custom drag-and-drop website design and development, eCommerce functionality, inventory management, shipping and logistics to build businesses and easily manage their online presence.\n\n2. Do I need coding experience to use WebManza?  \n\n No.\n\n3. How can I onboard with WebManza?\n With WebManza, you will have your site immediately! No delay, no waiting. Visit: www.webmanza.com/store-create and complete your WebManza subscription. You will instantly get access to your admin panel to start working on your website.`
   ),
 ];
 
