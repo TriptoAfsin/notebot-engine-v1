cdcb3e7b49a048e5a4e1e3f8c7865e92b07dcb86
TriptoAfsin <afsintripto@gmail.com>
2023-07-13 14:57:56 +0600

    chore: remove jQuery

--- src/public/js/profile.js
+++ src/public/js/profile.js
@@ -1,21 +1,42 @@
+window.addEventListener("load", () => {
+  const submitBtn = document.getElementById("submitFacebookProfile");
 
-window.addEventListener('load', ()=> {
-    const submitBtn = document.getElementById("submitFacebookProfile");
+  submitBtn.addEventListener("click", async () => {
+    event.preventDefault();
 
-    submitBtn.addEventListener("click", ()=> {
-        event.preventDefault();
-        
-        $.ajax({
-            url: `${window.location.origin}/set-up-user-fb-profile`,
-            method: "POST",
-            data: {},
-            success: function(data){
-                alert("Setup success");
-                console.log(data);
-            },
-            error: function(error){
-                console.log(error);
-            }
-        })
-    })
-})
+    //rewrite in fetch
+    try {
+      const response = await fetch(
+        `${window.location.origin}/set-up-user-fb-profile`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: {},
+        }
+      );
+      const result = await response.json();
+      if (result) {
+        alert("Setup success 😀");
+        console.log("Success:", result);
+      }
+    } catch (error) {
+      alert("Setup failed 🤔");
+      console.log(error);
+    }
+
+    // $.ajax({
+    //     url: `${window.location.origin}/set-up-user-fb-profile`,
+    //     method: "POST",
+    //     data: {},
+    //     success: function(data){
+    //         alert("Setup success");
+    //         console.log(data);
+    //     },
+    //     error: function(error){
+    //         console.log(error);
+    //     }
+    // })
+  });
+});
