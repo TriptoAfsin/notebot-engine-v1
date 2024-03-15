7cf98363d15ba2ce63525e8a4ae143f6ffdf26b6
TriptoAfsin <AfsinTripto@gmail.com>
2024-03-15 11:30:52 +0600

    fix: ap1 double file

--- src/controllers/appController/academic/labReport/level1/subs/ap1/ap1Labs.js
+++ src/controllers/appController/academic/labReport/level1/subs/ap1/ap1Labs.js
@@ -1,4 +1,4 @@
-let ap1LabFlow = require('../../../../../../flows/botReplies/lab_levels/level_1/level_1_lab_subs/ap_1/ap1LabFlow')
+let ap1LabFlow = require('../../../../../../flows/botReplies/lab_levels/level_2/level_2_lab_subs/ap_1/ap1LabFlow')
 let SubTopicTrans = require('../../../../../translaters/SubTopicTrans')
 
 
--- src/controllers/flows/botReplies/lab_levels/level_2/level_2_lab_subs/ap_1/ap1LabFlow.js
+++ src/controllers/flows/botReplies/lab_levels/level_2/level_2_lab_subs/ap_1/ap1LabFlow.js
@@ -50,6 +50,48 @@ let ap1_lab_flow = [
                 ],
             },
         },
+    },
+    {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "button",
+                "text": "🔰 Select Experiment for AP-I- ",
+                "buttons": [
+                    {
+                        "type": "postback",
+                        "title": "Attaching Interli.",
+                        "payload": "ap1_lab_attachingInterline_flow"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Standard Body Mea.",
+                        "payload": "ap1_lab_standardBody_flow"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Continuous Fusing MC",
+                        "payload": "ap1_lab_continuousFusing_flow"
+                    }
+                ],
+            },
+        },
+    },
+    {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "button",
+                "text": "🔰 Select Experiment for AP-I-  ",
+                "buttons": [
+                    {
+                        "type": "postback",
+                        "title": "Staright Knife & ",
+                        "payload": "ap1_lab_straightKnife_flow"
+                    }
+                ],
+            },
+        },
     }
 ]
 
