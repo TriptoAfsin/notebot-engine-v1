a57cd0b3df1c8d54318cf6dfa81f8313d9fe605d
TriptoAfsin <AfsinTripto@gmail.com>
2024-05-21 13:39:48 +0600

    content: mp lab app flow ++

--- src/controllers/flows/botReplies/lab_levels/level_2/level_2_lab_subs/mp/mpLabsFlowApp.js
+++ src/controllers/flows/botReplies/lab_levels/level_2/level_2_lab_subs/mp/mpLabsFlowApp.js
@@ -14,6 +14,8 @@ let mp_lab_flow = [
         "📌 All Reports -",
         [
             webBtnBlockGen("Download", "https://drive.google.com/file/d/1GtWZdRxrljTM4YSqNdNuoJkR1vAMohAU/view?usp=sharing"),
+            webBtnBlockGen("Jabbar, 2023", "https://drive.google.com/file/d/126Xoa2LTUKrhM1BtYXIy-VN73pYESEu7/view?usp=sharing"),
+            webBtnBlockGen("Himel, 2024", "https://drive.google.com/file/d/1Opk6Fa7qO9-2xpQlWXQNsKVprecO1Mf4/view?usp=sharing"),
         ]
     ),
 ]
