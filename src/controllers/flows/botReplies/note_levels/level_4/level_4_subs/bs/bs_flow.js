ceba1c94306dde36bf87d6e7c133cfeee31b59c4
TriptoAfsin <afsintripto@gmail.com>
2025-08-24 13:14:29 +0600

    feat: refactor Level 4 flow to use grouped button generation
    - Replace static button definitions with dynamic grouped button generation for improved maintainability
    - Enhance user experience by streamlining topic selection with new button structures
    - Maintain existing links while improving code readability and organization

--- src/controllers/flows/botReplies/note_levels/level_4/level_4_subs/bs/bs_flow.js
+++ src/controllers/flows/botReplies/note_levels/level_4/level_4_subs/bs/bs_flow.js
@@ -1,150 +1,59 @@
+let grroupedButtonBlockGen = require("simple-messenger-blocks/groupedBtnBlockGen");
+let webBtnBlockGen = require("simple-messenger-blocks/webBtnGen");
+
 let bsFlow = [
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Choose Topic - ",
-                "buttons": [
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/10L2QW-RDtiwkTHHoonqlgrMaRnKx8HH3/view",
-                        "title": "Bangladesh",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1Jz0Un7asZfl6FZOrusP8EQqM73CgcTQ9/view",
-                        "title": "Art & Culture",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1MI6Klqh6eUcWP5UvpowokF3q-zvpL2bP/view",
-                        "title": "Govn. Structure",
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Choose Topic - ",
-                "buttons": [
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1QjbEl4kkJ7YNvnFAHLsin3X1KtM1VqRJ/view",
-                        "title": "Constitution",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1ktuEUtk_62KgtKWgjmUbgxPwfB8mMHC_/view",
-                        "title": "Historical Backgroud",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1fddkK9Dn-yz4Tu6eYCZ7q-2yFggVqIO1/view",
-                        "title": "Local Govn.",
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Choose Topic - ",
-                "buttons": [
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1hru9rN4-nFoARmzFqn0h_LkrOwBTXf2Y/view",
-                        "title": "Organ",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1IYf_Qt2m8VsRV859vK_oIZsmDcEoLo7F/view",
-                        "title": "Climate Change Cause",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1_AQWPIgnTmSXjDo7cO6gMelv-aQIzWfL/view",
-                        "title": "ECNEC",
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Choose Topic - ",
-                "buttons": [
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1crx1mPuf0YTfylmcp6nkUukqqkvlz-HV/view",
-                        "title": "Environ. Pollution",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1iXupFhK6YoL3LW23EUhNvZyHa7NVRqcE/view",
-                        "title": "GDP & GNP",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1F22GKH-o4i3te6rJQ2nyVPBWPrYIrkMZ/view",
-                        "title": "Globalization",
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Choose Topic - ",
-                "buttons": [
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1ZX_1Qx5BvykCkrAdddfL7t6S2NmpJc2F/view",
-                        "title": "Good Govern..",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/18ZnT1PySWPS5-xhgDIm1sNzpD4A_kd8b/view",
-                        "title": "Overpopulation",
-                    },
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/1mfa8JO3eAMU5T4k4ABNahW2aS2J9ZPdg/view",
-                        "title": "RMG",
-                    }
-                ],
-            },
-        },
-    },
-    {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "🔰 Choose Topic - ",
-                "buttons": [
-                    {
-                        "type": "web_url",
-                        "url": "https://drive.google.com/file/d/14QJbiqMStK2A42QnCYsaV1-hyb3cE67v/view",
-                        "title": "Role of Political..",
-                    }
-                ],
-            },
-        },
-    }
+    grroupedButtonBlockGen(
+		"🔰 Hand Notes ",
+		[
+			webBtnBlockGen("2025", "https://drive.google.com/drive/folders/1LbuWc4de4-1m8Ybke15vuPQVdICLa3a5?usp=sharing"),
+		]
+	),
+	grroupedButtonBlockGen(
+		"🔰 Choose Topic - ",
+		[
+			webBtnBlockGen("Bangladesh", "https://drive.google.com/file/d/10L2QW-RDtiwkTHHoonqlgrMaRnKx8HH3/view"),
+			webBtnBlockGen("Art & Culture", "https://drive.google.com/file/d/1Jz0Un7asZfl6FZOrusP8EQqM73CgcTQ9/view"),
+			webBtnBlockGen("Govn. Structure", "https://drive.google.com/file/d/1MI6Klqh6eUcWP5UvpowokF3q-zvpL2bP/view"),
+		]
+	),
+	grroupedButtonBlockGen(
+		"🔰 Choose Topic - ",
+		[
+			webBtnBlockGen("Constitution", "https://drive.google.com/file/d/1QjbEl4kkJ7YNvnFAHLsin3X1KtM1VqRJ/view"),
+			webBtnBlockGen("Historical Backgroud", "https://drive.google.com/file/d/1ktuEUtk_62KgtKWgjmUbgxPwfB8mMHC_/view"),
+			webBtnBlockGen("Local Govn.", "https://drive.google.com/file/d/1fddkK9Dn-yz4Tu6eYCZ7q-2yFggVqIO1/view"),
+		]
+	),
+	grroupedButtonBlockGen(
+		"🔰 Choose Topic - ",
+		[
+			webBtnBlockGen("Organ", "https://drive.google.com/file/d/1hru9rN4-nFoARmzFqn0h_LkrOwBTXf2Y/view"),
+			webBtnBlockGen("Climate Change Cause", "https://drive.google.com/file/d/1IYf_Qt2m8VsRV859vK_oIZsmDcEoLo7F/view"),
+			webBtnBlockGen("ECNEC", "https://drive.google.com/file/d/1_AQWPIgnTmSXjDo7cO6gMelv-aQIzWfL/view"),
+		]
+	),
+	grroupedButtonBlockGen(
+		"🔰 Choose Topic - ",
+		[
+			webBtnBlockGen("Environ. Pollution", "https://drive.google.com/file/d/1crx1mPuf0YTfylmcp6nkUukqqkvlz-HV/view"),
+			webBtnBlockGen("GDP & GNP", "https://drive.google.com/file/d/1iXupFhK6YoL3LW23EUhNvZyHa7NVRqcE/view"),
+			webBtnBlockGen("Globalization", "https://drive.google.com/file/d/1F22GKH-o4i3te6rJQ2nyVPBWPrYIrkMZ/view"),
+		]
+	),
+	grroupedButtonBlockGen(
+		"🔰 Choose Topic - ",
+		[
+			webBtnBlockGen("Good Govern..", "https://drive.google.com/file/d/1ZX_1Qx5BvykCkrAdddfL7t6S2NmpJc2F/view"),
+			webBtnBlockGen("Overpopulation", "https://drive.google.com/file/d/18ZnT1PySWPS5-xhgDIm1sNzpD4A_kd8b/view"),
+			webBtnBlockGen("RMG", "https://drive.google.com/file/d/1mfa8JO3eAMU5T4k4ABNahW2aS2J9ZPdg/view"),
+		]
+	),
+	grroupedButtonBlockGen(
+		"🔰 Choose Topic - ",
+		[
+			webBtnBlockGen("Role of Political..", "https://drive.google.com/file/d/14QJbiqMStK2A42QnCYsaV1-hyb3cE67v/view"),
+		]
+	),
 ]
 
 
