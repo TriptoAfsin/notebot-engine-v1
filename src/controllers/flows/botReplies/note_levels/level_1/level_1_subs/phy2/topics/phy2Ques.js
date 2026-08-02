91f162a2e12130172235b6ab25394ccb1ca99fc8
TriptoAfsin <afsintripto@gmail.com>
2026-08-02 11:59:16 +0600

    ingest: Physics-2 Electricity QB solve (Saikot, YE-Affli) -> phy2Ques

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/phy2/topics/phy2Ques.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/phy2/topics/phy2Ques.js
@@ -3,6 +3,7 @@ let textBlockGen = require('simple-messenger-blocks/textBlockGen');
 let phy2Ques = [
     textBlockGen(`🔷 PHY II Questions( 2011 - 18)- \n\nhttps://drive.google.com/file/d/1v1U1Kw1_jhOUh1lMAWu_p3zP6PhEoFDT/view?usp=drivesdk`),
     textBlockGen(`🔷 Phy Affli Questions(2015 to 20)- \n\nhttps://drive.google.com/file/d/1Qm5oC2EceUjDsnxKe73oCOD7EDZddptU/view?usp=sharing`),
+    textBlockGen(`🔷 Electricity QB Solve(20-24)- Saikot(YE-Aff,26)\n\nhttps://drive.google.com/file/d/13P9ie4aagsJwqSq1pEL4AX5RQXiPD_C2/view?usp=sharing`),
 ]
 
 
