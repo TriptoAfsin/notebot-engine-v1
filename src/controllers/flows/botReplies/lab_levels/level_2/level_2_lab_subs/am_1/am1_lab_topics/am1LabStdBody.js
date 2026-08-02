eed38d3a21e2036552b5b8b3cf71480a469a193c
Afshin Nahian Tripto <AfsinTripto@gmail.com>
2026-08-02 12:30:55 +0600

    fix(ingest): id 158 is a lab report, not a note — move to am1 Standard Body Me. lab topic

--- src/controllers/flows/botReplies/lab_levels/level_2/level_2_lab_subs/am_1/am1_lab_topics/am1LabStdBody.js
+++ src/controllers/flows/botReplies/lab_levels/level_2/level_2_lab_subs/am_1/am1_lab_topics/am1LabStdBody.js
@@ -1,6 +1,9 @@
 let am1LabStdBody = [
     {
         "text": `🔷 Study on Standard Body Measurement(Akib ,2018)-\n\nhttps://drive.google.com/file/d/1DPuPQXIp5QC6aAq89Qp1w5gSNBA2DtMX/view`
+    },
+    {
+        "text": `🔷 Study on Standard Body Measurement(Midul, WPE-51, 2026)-\n\nhttps://drive.google.com/file/d/1-OfJmj0iseLcDIYMJUbmxN7OH4TXzALk/view?usp=sharing`
     }
 ]
 
--- src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/am1/am1_flow.js
+++ src/controllers/flows/botReplies/note_levels/level_2/level_2_subs/am1/am1_flow.js
@@ -62,13 +62,7 @@ let am1Flow = [
           
         ]
     ),
-    
-    grroupedButtonBlockGen(
-        "📌 Body Measurement -",
-        [
-            webBtnBlockGen("Midul(2026)", "https://drive.google.com/file/d/1-OfJmj0iseLcDIYMJUbmxN7OH4TXzALk/view?usp=sharing"),
-        ]
-    ),
+
     grroupedButtonBlockGen(
         "🔰 Select Topics for AM-I -",
         [
