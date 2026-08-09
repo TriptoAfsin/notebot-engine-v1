4ab590ca51b2b1718d80409e9c7c364736f04545
TriptoAfsin <afsintripto@gmail.com>
2026-08-09 23:26:41 +0600

    fix(fpc): correct spacing in button label for Polymer Characterization

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/fpc/fpc_flow.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/fpc/fpc_flow.js
@@ -8,7 +8,7 @@ let fpc_flow = [
     grroupedButtonBlockGen(
         "🔰 Select Topics for FPC - ",
         [
-            webBtnBlockGen("PolymerCharacterization(Jeba,2026)", "https://drive.google.com/file/d/1wxtc7I2hOCeqMPhT1GmKVOTAJk39ZHzY/view?usp=sharing"),
+            webBtnBlockGen("Polymer Characterization(Jeba,2026)", "https://drive.google.com/file/d/1wxtc7I2hOCeqMPhT1GmKVOTAJk39ZHzY/view?usp=sharing"),
             webBtnBlockGen("Conductive Polymer(Jeba,2026)", "https://drive.google.com/file/d/1BEdCQZ_MP2oRD8Zr4iCJR0jN2qa-OlOc/view?usp=sharing"),
             payloadBtnGen("Polymer Degrad.", "fpc_degrad_flow"),
         ]
