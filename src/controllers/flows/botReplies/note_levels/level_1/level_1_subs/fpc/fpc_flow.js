9868db916ef1261c5ddb068cb678c13acb48d1b1
TriptoAfsin <afsintripto@gmail.com>
2026-08-09 23:26:06 +0600

    feat(fpc): consolidate FPC topic buttons into a single grouped button block

--- src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/fpc/fpc_flow.js
+++ src/controllers/flows/botReplies/note_levels/level_1/level_1_subs/fpc/fpc_flow.js
@@ -6,20 +6,10 @@ let payloadBtnGen = require("simple-messenger-blocks/payloadBtnGen");
 
 let fpc_flow = [
     grroupedButtonBlockGen(
-        "📌 Conductive Polymer - ",
-        [
-            webBtnBlockGen("Jeba(2026)", "https://drive.google.com/file/d/1BEdCQZ_MP2oRD8Zr4iCJR0jN2qa-OlOc/view?usp=sharing"),
-        ]
-    ),
-    grroupedButtonBlockGen(
-        "📌 Characterization - ",
-        [
-            webBtnBlockGen("Jeba(2026)", "https://drive.google.com/file/d/1wxtc7I2hOCeqMPhT1GmKVOTAJk39ZHzY/view?usp=sharing"),
-        ]
-    ),
-    grroupedButtonBlockGen(
         "🔰 Select Topics for FPC - ",
         [
+            webBtnBlockGen("PolymerCharacterization(Jeba,2026)", "https://drive.google.com/file/d/1wxtc7I2hOCeqMPhT1GmKVOTAJk39ZHzY/view?usp=sharing"),
+            webBtnBlockGen("Conductive Polymer(Jeba,2026)", "https://drive.google.com/file/d/1BEdCQZ_MP2oRD8Zr4iCJR0jN2qa-OlOc/view?usp=sharing"),
             payloadBtnGen("Polymer Degrad.", "fpc_degrad_flow"),
         ]
     ),
